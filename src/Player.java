e5457e80651eff93963558aef9bbc611dd81f9db
JhonyRetro <94227499+JhonyRetro@users.noreply.github.com>
2024-09-26 09:14:10 +0000

    feat: add class constructors

--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,13 @@
 public class Player {
+    private final double DEF_SCORE = 0.0;
+
+    private String name;
+    private double score;
+    
+    Player(String newName){
+        this.name = newName;
+        this.score = DEF_SCORE;
+    }
+    
+
 }
--- src/PlayerList.java
+++ src/PlayerList.java
@@ -1,2 +1,10 @@
+import java.util.LinkedList;
+
 public class PlayerList {
+    private LinkedList<Player> players;
+
+    PlayerList(){
+        this.players = new LinkedList<Player>();
+    }
+    
 }
