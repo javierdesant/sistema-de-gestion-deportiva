2b41b4949d234162706ef8a99acffb41dff1fbf9
JhonyRetro <94227499+JhonyRetro@users.noreply.github.com>
2024-09-26 18:27:44 +0000

    feat: add public methods to Match class

--- src/Match.java
+++ src/Match.java
@@ -1,2 +1,20 @@
 public class Match {
+    private Player homePlayer;
+    private Player visitingPlayer;
+
+    Match(Player homePlayer, Player visitingPlayer){
+        this.homePlayer = homePlayer;
+        this.visitingPlayer = visitingPlayer;
+    }
+
+    public Player getHome(){
+        return this.homePlayer;
+    }
+    public Player getVisiting(){
+        return this.visitingPlayer;
+    }
+
+    public boolean isMatched(String playerName){
+        return homePlayer.getName() == playerName || visitingPlayer.getName() == playerName;
+    }
 }
