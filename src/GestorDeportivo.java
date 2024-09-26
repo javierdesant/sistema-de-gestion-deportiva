b6b095c023fd3ab16eb9ad714606e9b0f69f69ce
JhonyRetro <94227499+JhonyRetro@users.noreply.github.com>
2024-09-26 10:01:40 +0000

    test: score command test

--- src/GestorDeportivo.java
+++ src/GestorDeportivo.java
@@ -1,5 +1,12 @@
 public class GestorDeportivo {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        
+        PlayerList playerList = new PlayerList();
+        playerList.create("Luisa");
+        playerList.show();
+        playerList.score("Luisa", 9.0);
+        playerList.show();
+        
     }
 }
