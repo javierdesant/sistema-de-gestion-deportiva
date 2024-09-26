11e0e5659e267e21c6c7e08ec122bceca9b30be8
Javier de Santiago <j.desantiagosoto@gmail.com>
2024-09-26 10:48:18 +0200

    feat: add classes

--- src/GestorDeportivo.java
+++ src/GestorDeportivo.java
@@ -1,4 +1,4 @@
-public class Main {
+public class GestorDeportivo {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
---
+++ src/PlayerList.java
@@ -0,0 +1,2 @@
+public class PlayerList {
+}
