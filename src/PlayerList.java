0001a54ad19967e55dfde1ed0c308ac1b4a7bc34
JhonyRetro <94227499+JhonyRetro@users.noreply.github.com>
2024-09-26 10:00:38 +0000

    feat: add public methods and getPlayer method to PlayerList class

--- src/PlayerList.java
+++ src/PlayerList.java
@@ -1,3 +1,4 @@
+import java.util.Iterator;
 import java.util.LinkedList;
 
 public class PlayerList {
@@ -6,5 +7,36 @@ public class PlayerList {
     PlayerList(){
         this.players = new LinkedList<Player>();
     }
+    
+    public void create(String playerName){
+        this.players.add(new Player(playerName));
+    }
+
+    public void remove(Player player){
+        this.players.remove(player);
+    }
+
+    private Player getPlayer(String playerName){
+        Iterator<Player> iterator = players.descendingIterator();
+        Player res = null;
+        while(iterator.hasNext() && res == null){
+            Player currentPlayer = iterator.next();
+            if(currentPlayer.getName() == playerName)
+                res = currentPlayer;
+        }   
+        return res;
+    }
+
+    public void score(String playerName, double score){
+        this.getPlayer(playerName).setScore(score);
+    }
+
+    public void show(){
+        Iterator<Player> iterator = players.descendingIterator();
+        while (iterator.hasNext()) {
+            iterator.next().showPlayer();
+        }
+    }
+
     
 }
