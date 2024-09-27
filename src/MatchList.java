4fb18dbb6846c0b9192c41fe6f4a9d9b30dcff3e
Javier de Santiago <j.desantiagosoto@gmail.com>
2024-09-27 11:17:51 +0200

    feat(MatchList): add randomize method

--- src/MatchList.java
+++ src/MatchList.java
@@ -1,9 +1,23 @@
+import java.util.Collections;
 import java.util.LinkedList;
 
 public class MatchList{
-    private LinkedList<Match> matches;
+    private final LinkedList<Match> matches;
 
     MatchList(){
         this.matches = new LinkedList<Match>();
+    }
+
+    public void randomize(PlayerList playerList){
+        LinkedList<Player> players = playerList.getPlayers();
+        if (players.size() % 2 == 0) {
+            LinkedList<Player> randomPlayers = new LinkedList<>(players);
+            Collections.shuffle(randomPlayers);
+            while (!randomPlayers.isEmpty()) {
+                this.matches.add(new Match(randomPlayers.pop(), randomPlayers.pop()));
+            }
+        } else {
+            throw new Error("Players amount must be even");
+        }
     }
 }
