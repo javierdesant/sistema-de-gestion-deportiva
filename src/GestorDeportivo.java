f86ae231a133c95f4fd31d8dc5c3294552206b07
JavierDeSant <j.desantiagosoto@gmail.com>
2024-09-28 19:01:36 +0200

    BREAKING CHANGE: remade GestorDeportivo class

--- src/GestorDeportivo.java
+++ src/GestorDeportivo.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class GestorDeportivo {
     public static void main(String[] args) {    
         
@@ -9,72 +11,203 @@ public class GestorDeportivo {
             lo que habrá que meter el código en un try-catch dentro del while.
         */
         
-        
         PlayerList playerList = new PlayerList();
         
-        playerList.create("Luisa");
-        playerList.score("Luisa", 9.0);
+        playerList.add(new Player("Luisa"));
+        playerList.score("Luisa", 4.5); 
 
-        playerList.create("Manuel");
+        playerList.add(new Player("Manuel"));
         playerList.score("Manuel", 2.7);
 
-        playerList.create("Kurt");
+        playerList.add(new Player("Kurt"));
         playerList.score("Kurt", 4.0);
-
-        playerList.create("Sofia");
+        
+        playerList.add(new Player("Sofia"));
         playerList.score("Sofia", 3.8);
-
-        playerList.create("Robert");
+        
+        playerList.add(new Player("Robert"));
         playerList.score("Robert", 3.8);
 
-        playerList.create("Paco");  // FIXME: Este jugador no debería ser creado antes del bucle
-        playerList.score("Paco", 6.2);
-
-        playerList.show();
-
         MatchList matchList = new MatchList();
-
-        matchList.randomize(playerList);
-        matchList.show_matchmake();
-
-        matchList.clear_matchmake();
-        matchList.show_matchmake();
         
-        matchList.randomize(playerList);
-        matchList.show_matchmake();
+        System.out.println("\n### Bienvenido al Gestor Deportivo ###\n");
 
-        System.out.println("-----------TESTS-----------");  // TODO: Implementar tests correctamente tras cambiar la app
+        System.out.println("Introduce un comando o escribe 'exit' para salir.");
+        System.out.println("Comandos disponibles:\n");
 
-        System.out.println("TEST 1. Adding existing player (Sofia):");
-        try {
-            playerList.create("Sofia");
-            System.out.println("This should not be printed");
-        } catch (Error error) {
-            System.out.println("This error is correct: " + error.getMessage() + "\n");
-        }
+        System.out.println(" help\n" +
+            " create [player]\n" +
+            " remove [player]\n" +
+            " show\n" +
+            " rank\n" +
+            " score [player];[score]\n" +
+            " show_matchmake\n" +
+            " clear_matchmake\n" +
+            " matchmake [player1];[player2]\n" +
+            " random_matchmake\n" + 
+            " exit");
 
-        try {
-            playerList.add(new Player("Sofia"));
-            System.out.println("This should not be printed");
-        } catch (Error error) {
-            System.out.println("This error is correct: " + error.getMessage() + "\n");
-        }
+        Scanner scanner = new Scanner(System.in);
+        boolean exit = false;
 
-        System.out.println("TEST 2. Trying to randomize a MatchList with uneven players:");
+        do {
+            try {
+                System.out.println();
+                System.out.print(" > ");
+                String[] splitCommand = scanner.next().trim().split(" ");   // FIXME: No detecta los argumentos
+                String commandName = splitCommand[0];
+                String[] commandArgs = splitCommand.length > 1 ? splitCommand[1].split(";") : new String[0];
+                System.out.println();
+
+                switch (commandName) {
+                    case "create":
+                        if (commandArgs.length != 1) {
+                            throw new Error("Argumentos no válidos");
+                        }
+                        playerList.add(new Player(commandArgs[0]));
+                        break;
+                    
+                    case "remove":
+                        if (commandArgs.length != 1) {
+                            throw new Error("Argumentos no válidos");
+                        }
+                        playerList.remove(new Player(commandArgs[0]));
+                        break;
+
+                    case "show":
+                        playerList.show();
+                        break;
+
+                    case "rank":
+                        playerList.rank();  // TODO: Implementar método rank en PlayerList
+                        break;
+
+                    case "score":
+                        if (commandArgs.length != 2) {
+                            throw new Error("Argumentos no válidos");
+                        }
+                        try {
+                            playerList.score(commandArgs[0], Double.parseDouble(commandArgs[1]));
+                        } catch (NumberFormatException e) {
+                            throw new Error("La puntuación debe ser un número");
+                        }
+                        break;
+
+                    case "show_matchmake":
+                        matchList.show();
+                        break;
+                    
+                    case "clear_matchmake":
+                        matchList.clear();
+                        break;
+
+                    case "matchmake":
+                        matchList.add(new Match(new Player(commandArgs[0]), new Player(commandArgs[1])));
+                        break;
+
+                    case "random_matchmake":
+                        matchList.randomize(playerList);
+                        break;
+
+                    case "exit":
+                        System.out.println("Cerrando...");
+                        exit = true;
+                        break;
+
+                    case "help":
+                        System.out.println("Comandos disponibles:\n");
+
+                        System.out.println(" help\n" +
+                            " create [player]\n" +
+                            " remove [player]\n" +
+                            " show\n" +
+                            " rank\n" +
+                            " score [player];[score]\n" +
+                            " show_matchmake\n" +
+                            " clear_matchmake\n" +
+                            " matchmake [player1];[player2]\n" +
+                            " random_matchmake\n" + 
+                            " exit");
+                        break;
+                
+                    default:
+                        throw new Error("Comando no válido");
+                }
+            } catch (Error error) {
+                System.out.println(error.getMessage() + ", por favor inténtalo de nuevo.");
+            }
+            
+        } while (!exit);
+    
+
+        // Código viejo
+        // TODO: borrar antes de entregar
+
+        // PlayerList playerList = new PlayerList();
         
-        Player testPlayer = new Player("Name");
-        playerList.add(testPlayer);
-        playerList.score("Name", 3.8);
-        try {    
-            playerList.show();
-            new MatchList().randomize(playerList);
-            System.out.println("This should not be printed");
-        } catch (Error error) {
-            System.out.println("This error is correct: " + error.getMessage() + "\n");
-        } finally {
-            playerList.remove(testPlayer);
-        }
+        // playerList.create("Luisa");
+        // playerList.score("Luisa", 9.0);
 
-        System.out.println("---------------------------");
+        // playerList.create("Manuel");
+        // playerList.score("Manuel", 2.7);
+
+        // playerList.create("Kurt");
+        // playerList.score("Kurt", 4.0);
+
+        // playerList.create("Sofia");
+        // playerList.score("Sofia", 3.8);
+
+        // playerList.create("Robert");
+        // playerList.score("Robert", 3.8);
+
+        // playerList.create("Paco");
+        // playerList.score("Paco", 6.2);
+
+        // playerList.show();
+
+        // MatchList matchList = new MatchList();
+
+        // matchList.randomize(playerList);
+        // matchList.show_matchmake();
+
+        // matchList.clear_matchmake();
+        // matchList.show_matchmake();
+        
+        // matchList.randomize(playerList);
+        // matchList.show_matchmake();
+
+        // System.out.println("-----------TESTS-----------");  // TODO: Implementar tests correctamente tras cambiar la app
+
+        // System.out.println("TEST 1. Adding existing player (Sofia):");
+        // try {
+        //     playerList.create("Sofia");
+        //     System.out.println("This should not be printed");
+        // } catch (Error error) {
+        //     System.out.println("This error is correct: " + error.getMessage() + "\n");
+        // }
+
+        // try {
+        //     playerList.add(new Player("Sofia"));
+        //     System.out.println("This should not be printed");
+        // } catch (Error error) {
+        //     System.out.println("This error is correct: " + error.getMessage() + "\n");
+        // }
+
+        // System.out.println("TEST 2. Trying to randomize a MatchList with uneven players:");
+        
+        // Player testPlayer = new Player("Name");
+        // playerList.add(testPlayer);
+        // playerList.score("Name", 3.8);
+        // try {    
+        //     playerList.show();
+        //     new MatchList().randomize(playerList);
+        //     System.out.println("This should not be printed");
+        // } catch (Error error) {
+        //     System.out.println("This error is correct: " + error.getMessage() + "\n");
+        // } finally {
+        //     playerList.remove(testPlayer);
+        // }
+
+        // System.out.println("---------------------------");
     }
 }
