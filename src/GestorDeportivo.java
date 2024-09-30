c7195c6b13c94660184a497eb9c624e41cbb1ff5
JavierDeSant <j.desantiagosoto@gmail.com>
2024-10-01 01:30:38 +0200

    refactor: invert if condition for better readability refactor: use while instead of do while chore: change messages to a more formal tone

--- src/GestorDeportivo.java
+++ src/GestorDeportivo.java
@@ -1,56 +1,47 @@
 import java.util.Scanner;
 
 public class GestorDeportivo {
-    public static void main(String[] args) {    
-        
-        /* 
-            TODO!: Hay que añadir Scanner para que el usuario pueda interactuar con el programa,
-            y poder introducir datos desde la consola. 
-            La idea es rodear toda la app en un bucle que se ejecute hasta que el usuario decida salir. 
-            No deben aparecer errores o excepciones que te hagan salir del programa, por
-            lo que habrá que meter el código en un try-catch dentro del while.
-        */
-        
+    public static void main(String[] args) {
         PlayerList playerList = new PlayerList();
-        
+
         playerList.add(new Player("Luisa"));
-        playerList.score("Luisa", 4.5); 
+        playerList.score("Luisa", 4.5);
 
         playerList.add(new Player("Manuel"));
         playerList.score("Manuel", 2.7);
 
         playerList.add(new Player("Kurt"));
         playerList.score("Kurt", 4.0);
-        
+
         playerList.add(new Player("Sofia"));
         playerList.score("Sofia", 3.8);
-        
+
         playerList.add(new Player("Robert"));
         playerList.score("Robert", 3.8);
 
         MatchList matchList = new MatchList();
-        
+
         System.out.println("\n### Bienvenido al Gestor Deportivo ###\n");
 
-        System.out.println("Introduce un comando o escribe 'exit' para salir.");
+        System.out.println("Introduzca un comando o escriba 'exit' para salir.");
         System.out.println("Comandos disponibles:\n");
 
         System.out.println(" help\n" +
-            " create [player]\n" +
-            " remove [player]\n" +
-            " show\n" +
-            " rank\n" +
-            " score [player];[score]\n" +
-            " show_matchmake\n" +
-            " clear_matchmake\n" +
-            " matchmake [player1];[player2]\n" +
-            " random_matchmake\n" + 
-            " exit");
+                " create [player]\n" +
+                " remove [player]\n" +
+                " show\n" +
+                " rank\n" +
+                " score [player];[score]\n" +
+                " show_matchmake\n" +
+                " clear_matchmake\n" +
+                " matchmake [player1];[player2]\n" +
+                " random_matchmake\n" +
+                " exit");
 
         Scanner scanner = new Scanner(System.in);
         boolean exit = false;
 
-        do {
+        while (!exit) {
             try {
                 System.out.println();
                 System.out.print(" > ");
@@ -61,24 +52,21 @@ public class GestorDeportivo {
 
                 switch (commandName) {
                     case "create":
-                        if (commandArgs.length != 1) {
+                        if (commandArgs.length == 1) {
+                            playerList.add(new Player(commandArgs[0]));
+                            System.out.println("Jugador añadido con éxito.");
+                        } else {
                             throw new Error("Argumentos no válidos");
                         }
-                        int length = playerList.getPlayers().size();
-                        playerList.add(new Player(commandArgs[0]));
-                        if(playerList.getPlayers().size() > length){
-                            System.out.println("Jugador creado con éxito");
-                        } else throw new Error("Error en la creación del jugador");
                         break;
-                    
+
                     case "remove":
-                        if (commandArgs.length != 1) {
+                        if (commandArgs.length == 1) {
+                            playerList.remove(new Player(commandArgs[0]));
+                            System.out.println("Jugador eliminado con éxito.");
+                        } else {
                             throw new Error("Argumentos no válidos");
                         }
-                        if (playerList.getPlayers().contains(new Player(commandArgs[0]))){
-                            playerList.remove(new Player(commandArgs[0]));
-                            System.out.println("Jugador borrado con éxito");
-                        } else throw new Error("El jugador no existe");
                         break;
 
                     case "show":
@@ -90,30 +78,37 @@ public class GestorDeportivo {
                         break;
 
                     case "score":
-                        if (commandArgs.length != 2) {
+                        if (commandArgs.length == 2) {
+                            try {
+                                playerList.score(commandArgs[0], Double.parseDouble(commandArgs[1]));
+                                System.out.println(
+                                        "La puntuación de " + commandArgs[0] + " ahora es " + commandArgs[1] + ".");
+                            } catch (NumberFormatException e) {
+                                throw new Error("La puntuación debe ser un número");
+                            }
+                        } else {
                             throw new Error("Argumentos no válidos");
-                        }
-                        try {
-                            playerList.score(commandArgs[0], Double.parseDouble(commandArgs[1]));
-                        } catch (NumberFormatException e) {
-                            throw new Error("La puntuación debe ser un número");
                         }
                         break;
 
                     case "show_matchmake":
                         matchList.show();
                         break;
-                    
+
                     case "clear_matchmake":
                         matchList.clear(playerList);
+                        System.out.println("Los emparejamientos han sido eliminados.");
                         break;
 
                     case "matchmake":
-                        if (commandArgs.length != 2) {
+                        if (commandArgs.length == 2) {
+                            matchList.add(playerList,
+                                    new Match(playerList, new Player(commandArgs[0]), new Player(commandArgs[1])));
+                            System.out.println("Los jugadores " + commandArgs[0] + " y " + commandArgs[1] + " han sido emparejados correctamente.");
+                        } else {
                             throw new Error("Argumentos no válidos");
                         }
-                        matchList.matchmake(playerList, commandArgs[0], commandArgs[1]);  
-                        break;                                                                              
+                        break;
 
                     case "random_matchmake":
                         matchList.randomize(playerList);
@@ -126,28 +121,28 @@ public class GestorDeportivo {
                         break;
 
                     case "help":
+                    case "h":
                         System.out.println("Comandos disponibles:\n");
 
                         System.out.println(" help\n" +
-                            " create [player]\n" +
-                            " remove [player]\n" +
-                            " show\n" +
-                            " rank\n" +
-                            " score [player];[score]\n" +
-                            " show_matchmake\n" +
-                            " clear_matchmake\n" +
-                            " matchmake [player1];[player2]\n" +
-                            " random_matchmake\n" + 
-                            " exit");
+                                " create [player]\n" +
+                                " remove [player]\n" +
+                                " show\n" +
+                                " rank\n" +
+                                " score [player];[score]\n" +
+                                " show_matchmake\n" +
+                                " clear_matchmake\n" +
+                                " matchmake [player1];[player2]\n" +
+                                " random_matchmake\n" +
+                                " exit");
                         break;
-                
+
                     default:
                         throw new Error("Comando no válido");
                 }
             } catch (Error error) {
-                System.out.println(error.getMessage() + ", por favor inténtalo de nuevo.");
+                System.out.println(error.getMessage() + ", por favor inténtelo de nuevo.");
             }
-            
-        } while (!exit);
+        }
     }
 }
