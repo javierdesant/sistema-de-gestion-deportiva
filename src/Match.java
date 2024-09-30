7beaca3923c08fc24703acf17fdc53f2c0b04e8a
JavierDeSant <j.desantiagosoto@gmail.com>
2024-10-01 01:35:12 +0200

    feat: override equals method in Match class feat: add PlayerList to constructor arguments feat: add error handling to Match constructor feat: change methods names for clarity

--- src/Match.java
+++ src/Match.java
@@ -2,21 +2,46 @@ public class Match {
     private Player homePlayer;
     private Player visitingPlayer;
 
-    Match(Player homePlayer, Player visitingPlayer){
-        this.homePlayer = homePlayer;
-        homePlayer.setMatched(true);
-        this.visitingPlayer = visitingPlayer;
-        visitingPlayer.setMatched(true);
+    Match(PlayerList playerList, Player homePlayer, Player visitingPlayer) {
+        if (!playerList.contains(homePlayer) && !playerList.contains(visitingPlayer)) {
+            throw new Error("Los jugadores no existen");
+        } else if (!playerList.contains(homePlayer)) {
+            throw new Error("El jugador local no existe");
+        } else if (!playerList.contains(visitingPlayer)) {
+            throw new Error("El jugador visitante no existe");
+        } else if (homePlayer.equals(visitingPlayer)) {
+            throw new Error("Los jugadores no pueden ser el mismo");
+        } else {
+            this.homePlayer = homePlayer;
+            this.visitingPlayer = visitingPlayer;
+        }
     }
 
-    public Player getHome(){
+    public Player getHomePlayer() {
         return this.homePlayer;
     }
-    public Player getVisiting(){
+
+    public Player getVisitingPlayer() {
         return this.visitingPlayer;
     }
-    public void showMatch(){
-        System.out.println("Jugador local: " + homePlayer.getName()
-                            + "\nJugador visitante: " + visitingPlayer.getName());
+
+    public void show() {
+        System.out.println("Jugador local: " + this.homePlayer.getName());
+        System.out.println("Jugador visitante: " + this.visitingPlayer.getName());
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (object == null || getClass() != object.getClass()) {
+            return false;
+        }
+        Match match = (Match) object;
+        Player homePlayer = match.getHomePlayer();
+        Player visitingPlayer = match.getVisitingPlayer();
+        return this.homePlayer.equals(homePlayer) && this.visitingPlayer.equals(visitingPlayer) ||
+                this.homePlayer.equals(visitingPlayer) && this.visitingPlayer.equals(homePlayer);
     }
 }
