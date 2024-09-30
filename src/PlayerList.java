c5ef2ae8d4dd8f05b07f7a20ebb4eb1e45226f65
JavierDeSant <j.desantiagosoto@gmail.com>
2024-10-01 01:46:07 +0200

    perf: simplify player score method fix(PlayerList): improve error handling refactor: format code

--- src/PlayerList.java
+++ src/PlayerList.java
@@ -5,72 +5,67 @@ import java.util.LinkedList;
 public class PlayerList {
     private final LinkedList<Player> players;
 
-    PlayerList(){
+    PlayerList() {
         this.players = new LinkedList<>();
     }
 
-    public void add(Player newPlayer){
-        if (this.players.contains(newPlayer)) {
-            throw new Error("Player already exists");
-        } else {
+    public void add(Player newPlayer) {
+        if (!this.players.contains(newPlayer)) {
             this.players.add(newPlayer);
+        } else {
+            throw new Error("El jugador ya existe");
         }
     }
 
-    public void remove(Player player){
-        this.players.remove(player);
+    public void remove(Player player) {
+        if (!this.players.remove(player)) {
+            throw new Error("El jugador no existe");
+        }
     }
-   
-    public void show(){
+
+    public void show() {
         System.out.println("-----LISTA DE JUGADORES-----");
-        Iterator<Player> iterator = players.listIterator();
-        while (iterator.hasNext()) {
-            iterator.next().showPlayer();
+        for (Player player : players) {
+            player.showPlayer();
         }
         System.out.println("----------------------------");
     }
-    
-    private Player getPlayer(String playerName){
-        Iterator<Player> iterator = players.listIterator();
-        Player res = null;
-        while(iterator.hasNext() && res == null){
-            Player currentPlayer = iterator.next();
-            if(currentPlayer.getName().equals(playerName))
-                res = currentPlayer;
-        }   
-        return res;
-    }
 
-    public LinkedList<Player> getPlayers(){
+    public LinkedList<Player> getPlayers() {
         return this.players;
     }
 
-    public void score(String playerName, double score){
-        this.getPlayer(playerName).setScore(score);
-        System.out.println("La puntuación de " + playerName + " ahora es " + score);
+    public void score(String playerName, double score) {    // TODO: implementar límite máximo, mínimo y de decimales
+        int index = this.players.indexOf(new Player(playerName));
+
+        if (index != -1) {
+            this.players.get(index).setScore(score);
+        } else {
+            throw new Error("El jugador no existe");
+        }
+    }
+
+    public boolean contains(Player player) {
+        return this.players.contains(player);
     }
 
     public void rank() {
         ArrayList<Player> playerList = new ArrayList<>(players);
-        for (int i=0; i < playerList.size()-1; i++) {
-            for (int j=0; j < playerList.size()-i-1; j++) {
-                if (playerList.get(j).getScore() < playerList.get(j+1).getScore()) {
+        for (int i = 0; i < playerList.size() - 1; i++) {
+            for (int j = 0; j < playerList.size() - i - 1; j++) {
+                if (playerList.get(j).getScore() < playerList.get(j + 1).getScore()) {
                     Player temp = playerList.get(j);
-                    playerList.set(j, playerList.get(j+1));
-                    playerList.set(j+1, temp);
+                    playerList.set(j, playerList.get(j + 1));
+                    playerList.set(j + 1, temp);
                 }
             }
         }
         System.out.println("----------RANKING-----------");
         Iterator<Player> iterator = playerList.iterator();
         while (iterator.hasNext()) {
-            Player currentPlayer = iterator.next();
-            currentPlayer.showPlayer();
-            currentPlayer.showRank();
+            iterator.next().show();
             System.out.println();
         }
         System.out.println("----------------------------");
-        
-    }    
-
+    }
 }
