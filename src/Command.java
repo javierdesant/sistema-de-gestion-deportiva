8966abeb8c17818ace4c1873df70be01e6ddf058
JhonyRetro <94227499+JhonyRetro@users.noreply.github.com>
2024-10-01 16:40:20 +0000

    feat: add public methods to Command class

--- src/Command.java
+++ src/Command.java
@@ -1,9 +1,28 @@
+import java.util.Scanner;
+
 public class Command {
     private String name;
     private String[] arguments;
+    private static final Scanner scanner = new Scanner(System.in);
 
-    Command(String name, String[] arguments) {
-        this.name = name;
-        this.arguments = arguments;
+    Command() {
+        this.name = null;
+        this.arguments = null;
+    }
+    public String getCommandName(){
+        return this.name;
+    }
+    public String getArgument(int index){
+        return this.arguments[index];
+    }
+    public int getArgumentsLength(){
+        return this.arguments.length;    
+    }
+    public Command readCommand(){
+        System.out.printf("> ");
+        String[] splitCommand = scanner.nextLine().trim().split(" ");
+        this.name = splitCommand[0];
+        this.arguments = splitCommand.length > 1 ? splitCommand[1].split(";") : new String[0];
+        return this;
     }
 }
