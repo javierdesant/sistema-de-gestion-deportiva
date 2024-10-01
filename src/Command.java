76164ed82a202fc803fa78f1cc7abb53cbe6ef2d
Javier de Santiago <j.desantiagosoto@gmail.com>
2024-10-01 12:54:02 +0200

    feat: add new Command class

---
+++ src/Command.java
@@ -0,0 +1,9 @@
+public class Command {
+    private String name;
+    private String[] arguments;
+
+    Command(String name, String[] arguments) {
+        this.name = name;
+        this.arguments = arguments;
+    }
+}
