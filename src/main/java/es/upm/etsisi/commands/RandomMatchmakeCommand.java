3d9f192b8737766cf0629a2f083ff8524bbdfce6
JhonyRetro <94227499+JhonyRetro@users.noreply.github.com>
2024-10-23 15:30:07 +0000

    refactor: remove completed TODO

--- src/main/java/es/upm/etsisi/commands/RandomMatchmakeCommand.java
+++ src/main/java/es/upm/etsisi/commands/RandomMatchmakeCommand.java
@@ -8,7 +8,7 @@ public class RandomMatchmakeCommand extends Command {
     }
 
     @Override
-    public void execute() {     // TODO: create warning menu class
+    public void execute() {
         if (getMatchList().isEmpty()) {
             getMatchList().randomize(getPlayerList());
             Message.MATCHES_RANDOMIZED.writeln();
