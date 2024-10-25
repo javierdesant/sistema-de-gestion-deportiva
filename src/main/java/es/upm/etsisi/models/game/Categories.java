27942a1a6a3b262d1d8f1ca10a53c500ee848b84
Javier de Santiago <j.desantiagosoto@gmail.com>
2024-10-25 22:23:55 +0200

    feat: add new Categories enum

---
+++ src/main/java/es/upm/etsisi/models/game/Categories.java
@@ -0,0 +1,9 @@
+package es.upm.etsisi.models.game;
+
+public enum Categories {
+    POINTS_SCORED,
+    MATCHES_WON,
+    ASSIST_POINTS,
+    TOURNAMENTS_WON,
+    MONEY_GENERATED_IN_THE_TOURNAMENT
+}
