23dbaf0c67fadb4f6b0adf02981f82621838dad5
Javier de Santiago <j.desantiagosoto@gmail.com>
2024-10-25 22:49:11 +0200

    chore(Statistics): add missing this.

--- src/main/java/es/upm/etsisi/models/game/Statistics.java
+++ src/main/java/es/upm/etsisi/models/game/Statistics.java
@@ -6,24 +6,24 @@ public class Statistics {
     private final EnumMap<Categories, Double> statistics;
 
     public Statistics() {
-        statistics = new EnumMap<>(Categories.class);
+        this.statistics = new EnumMap<>(Categories.class);
         for (Categories category : Categories.values()) {
-            statistics.put(category, 0.0);
+            this.statistics.put(category, 0.0);
         }
     }
 
     public void setStatistic(Categories category, double value) {
-        statistics.put(category, value);
+        this.statistics.put(category, value);
     }
 
     public double getStatistic(Categories category) {
-        assert statistics.containsKey(category) : "No se puede obtener el valor de la categoria";
+        assert this.statistics.containsKey(category) : "No se puede obtener el valor de la categoria";
 
-        return statistics.get(category);
+        return this.statistics.get(category);
     }
 
     public EnumMap<Categories, Double> getAllStatistics() {
-        return statistics;
+        return this.statistics;
     }
 
     public void writeln() {
