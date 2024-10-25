b787575ee2911a9bc1b075aa6aba4f9632408a55
Javier de Santiago <j.desantiagosoto@gmail.com>
2024-10-25 14:22:16 +0200

    chore(Command): add missing annotations

--- src/main/java/es/upm/etsisi/commands/Command.java
+++ src/main/java/es/upm/etsisi/commands/Command.java
@@ -22,7 +22,10 @@ public abstract class Command implements Item {
         return this.arguments[index];
     }
 
+    @Override
     public void validate(String input) {
+        assert input != null;
+
         String[] split = input.split(" ");
         this.name = split[0];
         if (split.length > 1) {
@@ -32,6 +35,7 @@ public abstract class Command implements Item {
         }
     }
 
+    @Override
     public boolean isCalled() {
         return this.name.equals(this.name_);
     }
