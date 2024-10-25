875e5d7c43ebe0d3abc0ce411c9b546b1f4cca7a
Javier de Santiago <j.desantiagosoto@gmail.com>
2024-10-25 22:24:34 +0200

    feat: define Statistics class with a Categories EnumMap

--- src/main/java/es/upm/etsisi/models/game/Statistics.java
+++ src/main/java/es/upm/etsisi/models/game/Statistics.java
@@ -1,4 +1,28 @@
 package es.upm.etsisi.models.game;
 
+import java.util.EnumMap;
+
 public class Statistics {
+    private final EnumMap<Categories, Double> statistics;
+
+    public Statistics() {
+        statistics = new EnumMap<>(Categories.class);
+        for (Categories category : Categories.values()) {
+            statistics.put(category, 0.0);
+        }
+    }
+
+    public void setStatistic(Categories category, double value) {
+        statistics.put(category, value);
+    }
+
+    public double getStatistic(Categories category) {
+        assert statistics.containsKey(category) : "No se puede obtener el valor de la categoria";
+
+        return statistics.get(category);
+    }
+
+    public EnumMap<Categories, Double> getAllStatistics() {
+        return statistics;
+    }
 }
