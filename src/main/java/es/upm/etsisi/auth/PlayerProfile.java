2ea23781efc0e3df7fd54b91de23f91acd643a0d
Javier de Santiago <j.desantiagosoto@gmail.com>
2024-10-26 11:08:15 +0200

    feat: start implementing auth models

--- src/main/java/es/upm/etsisi/auth/PlayerProfile.java
+++ src/main/java/es/upm/etsisi/auth/PlayerProfile.java
@@ -1,4 +1,30 @@
 package es.upm.etsisi.auth;
 
-public class PlayerProfile {
+public class PlayerProfile implements User {
+    private String name;
+    private String password;
+
+    public PlayerProfile(String name, String password) {
+        this.name = name;   // TODO: validate for upm email
+        this.password = password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.name;
+    }
+
+    @Override
+    public void setUsername(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void setPassword(String oldPassword, String newPassword) {
+        assert oldPassword != null;
+        assert newPassword != null;
+        assert oldPassword.equals(this.password);   // TODO: add message
+
+        this.password = newPassword;
+    }
 }
