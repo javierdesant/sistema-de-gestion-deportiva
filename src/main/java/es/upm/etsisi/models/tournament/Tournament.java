79e165ce0cdbcd04085f40b4c57642595465aff7
Javier de Santiago <j.desantiagosoto@gmail.com>
2024-10-26 11:58:16 +0200

    feat: add some attributes to Tournament class

--- src/main/java/es/upm/etsisi/models/tournament/Tournament.java
+++ src/main/java/es/upm/etsisi/models/tournament/Tournament.java
@@ -1,13 +1,26 @@
 package es.upm.etsisi.models.tournament;
 
+import es.upm.etsisi.models.game.Category;
 import es.upm.etsisi.models.game.MatchList;
+import es.upm.etsisi.models.game.Sport;
+
+import java.util.Date;
 
 public class Tournament {
+    private final String name;
+    private final Date startDate;
+    private final Date endDate;
+    private final Sport sport;
+    // private final League league;   // TODO
+    private final Category category;
     private final MatchList matchList;
 
-    public Tournament(MatchList matchList) {
+    public Tournament(String name, Date startDate, Date endDate, Sport sport, Category category, MatchList matchList) {
+        this.name = name;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.sport = sport;
+        this.category = category;
         this.matchList = matchList;
     }
-
-
 }
