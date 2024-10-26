c2e7432b57a4fb52a4a5ec3a5b199bba24595756
Javier de Santiago <j.desantiagosoto@gmail.com>
2024-10-26 11:47:51 +0200

    feat: add Sports enum to models.game

---
+++ src/main/java/es/upm/etsisi/models/game/Sports.java
@@ -0,0 +1,55 @@
+package es.upm.etsisi.models.game;
+
+public enum Sports {
+    SOCCER("Fútbol"),
+    BASKETBALL("Baloncesto"),
+    TENNIS("Tenis"),
+    SWIMMING("Natación"),
+    BOXING("Boxeo"),
+    ATHLETICS("Atletismo"),
+    GOLF("Golf"),
+    CRICKET("Críquet"),
+    BASEBALL("Béisbol"),
+    RUGBY("Rugby"),
+    VOLLEYBALL("Voleibol"),
+    HANDBALL("Balonmano"),
+    CYCLING("Ciclismo"),
+    HOCKEY("Hockey"),
+    SKIING("Esquí"),
+    SNOWBOARDING("Snowboard"),
+    ICE_HOCKEY("Hockey sobre hielo"),
+    SURFING("Surf"),
+    BADMINTON("Bádminton"),
+    TABLE_TENNIS("Tenis de mesa"),
+    WRESTLING("Lucha"),
+    JUDO("Judo"),
+    KARATE("Karate"),
+    TAEKWONDO("Taekwondo"),
+    FENCING("Esgrima"),
+    ARCHERY("Tiro con arco"),
+    SHOOTING("Tiro"),
+    EQUESTRIAN("Equitación"),
+    TRIATHLON("Triatlón"),
+    WEIGHTLIFTING("Halterofilia"),
+    SAILING("Vela"),
+    ROWING("Remo"),
+    CANOEING("Piragüismo"),
+    SKATEBOARDING("Monopatín"),
+    CLIMBING("Escalada"),
+    GYMNASTICS("Gimnasia"),
+    MARTIAL_ARTS("Artes marciales"),
+    POLO("Polo"),
+    MOTORSPORTS("Deportes de motor"),
+    ESPORTS("Esports");
+
+    private final String name;
+
+    Sports(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
