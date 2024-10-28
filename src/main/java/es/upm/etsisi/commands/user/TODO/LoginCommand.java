a0f627f482c1ea31ab4022a34a00c1e266aece08
Javier de Santiago <j.desantiagosoto@gmail.com>
2024-10-28 12:26:49 +0100

    feat: define LoginCommand class

--- src/main/java/es/upm/etsisi/commands/user/TODO/LoginCommand.java
+++ src/main/java/es/upm/etsisi/commands/user/TODO/LoginCommand.java
@@ -1,4 +1,30 @@
 package es.upm.etsisi.commands.user.TODO;
 
-public class LoginCommand {
+import es.upm.etsisi.auth.User;
+import es.upm.etsisi.auth.UserList;
+import es.upm.etsisi.commands.Command;
+
+public class LoginCommand extends Command {
+    private User user;
+    private final UserList userList;
+
+    public LoginCommand(User user, UserList userList) {
+        super("login");
+        this.user = user;
+        this.userList = userList;
+    }
+
+    @Override
+    public void execute() {
+        assert this.user == null;   // TODO: add message
+
+        String username = getArgument(0);
+        String password = getArgument(1);
+        User user = this.userList.getByUsername(username);
+
+        assert user != null;
+        assert user.validate(password);
+
+        this.user = user;
+    }
 }
