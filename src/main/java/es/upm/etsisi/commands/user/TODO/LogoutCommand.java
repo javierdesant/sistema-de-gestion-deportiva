b2e03d395e8480599d2aebbd9f81433e7bb9c82c
Javier de Santiago <j.desantiagosoto@gmail.com>
2024-10-28 12:33:03 +0100

    feat: define LogoutCommand

--- src/main/java/es/upm/etsisi/commands/user/TODO/LogoutCommand.java
+++ src/main/java/es/upm/etsisi/commands/user/TODO/LogoutCommand.java
@@ -1,4 +1,20 @@
 package es.upm.etsisi.commands.user.TODO;
 
-public class LogoutCommand {
+import es.upm.etsisi.auth.User;
+import es.upm.etsisi.commands.Command;
+
+public class LogoutCommand extends Command {
+    private User user;
+
+    public LogoutCommand(User user) {
+        super("login");
+        this.user = user;
+    }
+
+    @Override
+    public void execute() {
+        assert this.user != null;   // TODO: add Message
+
+        this.user = null;
+    }
 }
