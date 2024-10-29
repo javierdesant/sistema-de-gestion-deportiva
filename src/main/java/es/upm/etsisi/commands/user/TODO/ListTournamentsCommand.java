de5b471fe2a64bba423da58aa5686859b03d2fe4
Javier de Santiago <j.desantiagosoto@gmail.com>
2024-10-29 12:27:32 +0100

    feat: define constructor of ListTournamentsCommand

--- src/main/java/es/upm/etsisi/commands/user/TODO/ListTournamentsCommand.java
+++ src/main/java/es/upm/etsisi/commands/user/TODO/ListTournamentsCommand.java
@@ -1,4 +1,18 @@
 package es.upm.etsisi.commands.user.TODO;
 
-public class ListTournamentsCommand {
+import es.upm.etsisi.commands.Command;
+import es.upm.etsisi.models.game.TournamentList;
+
+public class ListTournamentsCommand extends Command {
+    TournamentList tournaments;
+
+    public ListTournamentsCommand(TournamentList tournaments) {
+        super("tournaments");
+        this.tournaments = tournaments;
+    }
+
+    @Override
+    public void execute() {
+        // TODO
+    }
 }
