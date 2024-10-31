7b60de78faead10f8a945b3329f7c2ba4737ca8f
Javier de Santiago <j.desantiagosoto@gmail.com>
2024-10-31 14:40:11 +0100

    feat(Entity)!: make it an abstract class and implement EntityView

--- src/main/java/es/upm/etsisi/models/entities/Entity.java
+++ src/main/java/es/upm/etsisi/models/entities/Entity.java
@@ -1,12 +1,39 @@
 package es.upm.etsisi.models.entities;
 
+import es.upm.etsisi.models.game.Category;
+import es.upm.etsisi.models.game.Statistics;
+import es.upm.etsisi.views.EntityView;
+
 import java.util.ArrayList;
 
-public interface Entity {
-    String getName();
+public abstract class Entity {
+    private final EntityView view;
+    private final String name;
+    private final Statistics stats;
 
-    void add(Entity entity);
-    void remove(Entity entity);
+    public Entity(String name, Statistics stats) {
+        this.view = new EntityView(this);
+        this.name = name;
+        this.stats = stats;
+    }
 
-    ArrayList<Entity> getChildren();
+    public String getName() {
+        return this.name;
+    }
+
+    public Statistics getStats() {
+        return this.stats;
+    }
+
+    public double getStat(Category category) {
+        return this.stats.get(category);
+    }
+
+    // TODO: setStats
+
+    public void show() {
+        this.view.display();
+    }
+
+    public abstract ArrayList<Entity> getChildren();    // should this be public ?
 }
