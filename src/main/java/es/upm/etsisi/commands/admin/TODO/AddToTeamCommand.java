535be2d30403e2cb937fab8a87204399518005f7
Javier de Santiago <j.desantiagosoto@gmail.com>
2024-11-05 20:13:09 +0100

    feat: define AddToTeamCommand

--- src/main/java/es/upm/etsisi/commands/admin/TODO/AddToTeamCommand.java
+++ src/main/java/es/upm/etsisi/commands/admin/TODO/AddToTeamCommand.java
@@ -1,18 +1,31 @@
 package es.upm.etsisi.commands.admin.TODO;
 
 import es.upm.etsisi.commands.Command;
+import es.upm.etsisi.models.entities.Entity;
 import es.upm.etsisi.models.entities.EntityList;
+import es.upm.etsisi.models.entities.Player;
+import es.upm.etsisi.models.entities.Team;
 
 public class AddToTeamCommand extends Command {
-    private EntityList entityList;
+    private final EntityList entityList;
 
     public AddToTeamCommand(EntityList entityList) {
-        super("team-add");
+        super("team-add", 2);
         this.entityList = entityList;
     }
 
     @Override
     public void execute() {
-        // TODO
+        String playerName = this.getArgument(0);
+        String teamName = this.getArgument(1);
+
+        Entity entity1 = this.entityList.getByName(teamName);
+        Entity entity2 = this.entityList.getByName(playerName);
+
+        if (entity1 instanceof Team team && entity2 instanceof Player player) {
+            team.add(player);
+        } else {
+            System.out.println("error: No se puede agregar el objeto");    // TODO: fix error display
+        }
     }
 }
