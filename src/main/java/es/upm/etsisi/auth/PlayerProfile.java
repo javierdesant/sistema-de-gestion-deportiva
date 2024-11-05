6b7fd51ce5bfa1ea049cd1d39e485e614c7f3532
Javier de Santiago <j.desantiagosoto@gmail.com>
2024-11-06 00:36:14 +0100

    feat!: add Player attribute to PlayerProfile

--- src/main/java/es/upm/etsisi/auth/PlayerProfile.java
+++ src/main/java/es/upm/etsisi/auth/PlayerProfile.java
@@ -1,21 +1,29 @@
 package es.upm.etsisi.auth;
 
+import es.upm.etsisi.models.entities.Player;
+import es.upm.etsisi.models.game.Statistics;
 import es.upm.etsisi.utils.DNI;
 
 public class PlayerProfile extends User {
     private final String firstName;
     private final String lastName;
     private final DNI dni;
+    private final Player player;
 
-    public PlayerProfile(String username, String password, DNI dni, String firstName, String lastName) {
+    public PlayerProfile(String username, String password, DNI dni, String firstName, String lastName, Player player) {
         super(username, password);
         this.dni = dni;
         this.firstName = firstName;
         this.lastName = lastName;
+        this.player = player;
     }
 
     public String getDni() {
         return this.dni.getValue();
+    }
+
+    public Statistics getStats() {
+        return this.player.getStats();
     }
 
     public void show() {
