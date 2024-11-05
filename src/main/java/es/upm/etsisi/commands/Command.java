d671a06add67fa0329099a469ea01e0947742d61
Javier de Santiago <j.desantiagosoto@gmail.com>
2024-11-05 12:11:02 +0100

    feat(Command)!: remove name_ attribute, add maxArguments attribute

--- src/main/java/es/upm/etsisi/commands/Command.java
+++ src/main/java/es/upm/etsisi/commands/Command.java
@@ -3,29 +3,35 @@ package es.upm.etsisi.commands;
 import es.upm.etsisi.utils.Message;
 
 public abstract class Command {
-    private final String name_;
-    private String name;
+    private final String name;
+    private final int maxArguments;
     private String[] arguments;
 
+    public Command(String name, int maxArguments) {
+        this.name = name;
+        this.maxArguments = maxArguments;
+    }
+
     public Command(String name) {
-        this.name_ = name;
+        this(name, 0);
     }
 
     public String getName() {
-        return this.name_;
+        return this.name;
     }
 
     protected String getArgument(int index) {     // TODO: añadir un máximo de args dependiendo del comando
         assert this.arguments.length > index : Message.INVALID_ARGUMENTS;
+        assert index < this.maxArguments : Message.INVALID_ARGUMENTS;
 
         return this.arguments[index];
     }
 
     public void validate(String input) {
-        assert input != null;
+        assert this.isCalled(input);
 
-        String[] split = input.split(" ");
-        this.name = split[0];
+        String[] split = input.trim().split(" ");
+
         if (split.length > 1) {
             this.arguments = split[1].split(";");
         } else {
@@ -33,8 +39,11 @@ public abstract class Command {
         }
     }
 
-    public boolean isCalled() {
-        return this.name.equals(this.name_);
+    public boolean isCalled(String input) {
+        assert input != null;
+
+        String name = input.trim().split(" ")[0];
+        return name.equals(this.name);
     }
 
     public abstract void execute();
