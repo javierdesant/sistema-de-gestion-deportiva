4d7c934f4dbe5b511e8323d652eebffa89a60489
Javier de Santiago <j.desantiagosoto@gmail.com>
2024-11-06 20:58:25 +0100

    feat: start defining TournamentList

--- src/main/java/es/upm/etsisi/models/game/TournamentList.java
+++ src/main/java/es/upm/etsisi/models/game/TournamentList.java
@@ -1,4 +1,28 @@
 package es.upm.etsisi.models.game;
 
-public class TournamentList {
+import es.upm.etsisi.service.List;
+import es.upm.etsisi.views.TournamentListView;
+
+public class TournamentList extends List<Tournament> {
+    private final TournamentListView view;
+
+    public TournamentList() {
+        super();
+        this.view = new TournamentListView(this);
+    }
+
+    @Override
+    public void add(Tournament element) {
+
+    }
+
+    @Override
+    public void remove(Tournament element) {
+
+    }
+
+    @Override
+    public void show() {
+        this.view.display();
+    }
 }
