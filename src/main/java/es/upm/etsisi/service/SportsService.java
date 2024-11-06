3001248587ecc65694d40f0bd246db3be928beab
Javier de Santiago <j.desantiagosoto@gmail.com>
2024-11-06 20:56:05 +0100

    feat(SportService): replace matchlist with tournamentlist

--- src/main/java/es/upm/etsisi/service/SportsService.java
+++ src/main/java/es/upm/etsisi/service/SportsService.java
@@ -3,20 +3,20 @@ package es.upm.etsisi.service;
 import es.upm.etsisi.auth.Administrator;
 import es.upm.etsisi.models.entities.EntityList;
 import es.upm.etsisi.models.entities.Player;
-import es.upm.etsisi.models.game.MatchList;
+import es.upm.etsisi.models.game.TournamentList;
 import es.upm.etsisi.utils.Status;
 
 public class SportsService {
     private final EntityList entityList;
-    private final MatchList matchList;
+    private final TournamentList tournamentList;
     private final CLI cli;
     private Status status;
 
     public SportsService() {
         this.status = Status.CLOSED;
         this.entityList = new EntityList();
-        this.matchList = new MatchList();
-        this.cli = new CLI(this.entityList, this.matchList, this);
+        this.tournamentList = new TournamentList();
+        this.cli = new CLI(this.entityList, this.tournamentList, this);
     }
 
     public boolean isOpen() {
@@ -48,6 +48,7 @@ public class SportsService {
 
         for (String defaultName : defaultNames) {
             this.entityList.add(new Player(defaultName, new Administrator("default@upm.es", "")));
+                // TODO: fix test auth
         }
     }
 
