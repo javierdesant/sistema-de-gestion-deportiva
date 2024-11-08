fe4f46e69009545cb998e79329152b5c4acbae8f
Javier de Santiago <j.desantiagosoto@gmail.com>
2024-11-08 12:04:05 +0100

    feat(Controller): add basic attributes

--- src/main/java/es/upm/etsisi/service/Controller.java
+++ src/main/java/es/upm/etsisi/service/Controller.java
@@ -1,4 +1,18 @@
 package es.upm.etsisi.service;
 
+import es.upm.etsisi.models.entities.ParticipantList;
+import es.upm.etsisi.models.game.TournamentList;
+
 public class Controller {
+    private final AuthController authController;
+    private final ParticipantList participantList;
+    private final TournamentList tournamentList;
+
+    public Controller() {
+        this.authController = new AuthController();
+        this.participantList = new ParticipantList();
+        this.tournamentList = new TournamentList();
+    }
+
+
 }
