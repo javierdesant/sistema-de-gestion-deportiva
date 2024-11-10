f3cd15d4cfd344055d0aaa8e42f40b3b71211d1c
Javier de Santiago <j.desantiagosoto@gmail.com>
2024-11-10 12:06:11 +0100

    fix: improve ShowMatchmakeCommmand

--- src/main/java/es/upm/etsisi/commands/TODO/ShowMatchmakeCommand.java
+++ src/main/java/es/upm/etsisi/commands/TODO/ShowMatchmakeCommand.java
@@ -1,22 +1,49 @@
 package es.upm.etsisi.commands.TODO;
 
 import es.upm.etsisi.commands.Command;
+import es.upm.etsisi.models.entities.Participant;
+import es.upm.etsisi.models.game.Match;
 import es.upm.etsisi.models.game.MatchList;
 import es.upm.etsisi.models.entities.ParticipantList;
 import es.upm.etsisi.utils.Message;
+
+import java.util.Iterator;
 
 public class ShowMatchmakeCommand extends Command {
     private final MatchList matchList;
 
     public ShowMatchmakeCommand(ParticipantList participantList, MatchList matchList) {
-        super("show_matchmake");
+        super("show_matchmake", 0);
         this.matchList = matchList;
     }
 
     @Override
     public void execute() {
         Message.MATCHMAKE_HEADER.writeln();
-        this.matchList.show();
+
+        if (this.matchList.isEmpty()) {
+            Message.NO_MATCHES.writeln();
+        } else {
+            for (Match match : this.matchList.getElements()) {
+                this.showMatch(match);
+            }
+        }
+
         Message.FOOTER.writeln();
+    }
+
+    private void showMatch(Match match) {
+        assert match != null;
+        assert match.getParticipants().size() > 1;
+
+        Iterator<Participant> iterator = match.getParticipants().iterator();
+
+        Message.LIGHT_FOOTER.writeln();
+        System.out.println(iterator.next());
+        do {
+            System.out.println("vs");
+            System.out.println(iterator.next());
+        } while (iterator.hasNext());
+        Message.LIGHT_FOOTER.writeln();
     }
 }
