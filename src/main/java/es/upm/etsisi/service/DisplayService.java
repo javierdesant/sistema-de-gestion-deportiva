81fd20260d2f5b962735f35893e0a569d97e073a
Javier de Santiago <j.desantiagosoto@gmail.com>
2024-11-10 13:07:10 +0100

    feat!: add DisplayService to format models

---
+++ src/main/java/es/upm/etsisi/service/DisplayService.java
@@ -0,0 +1,55 @@
+package es.upm.etsisi.service;
+
+import es.upm.etsisi.models.entities.Participant;
+import es.upm.etsisi.models.game.Category;
+import es.upm.etsisi.models.game.Match;
+import es.upm.etsisi.models.game.Statistics;
+import es.upm.etsisi.models.game.Tournament;
+import es.upm.etsisi.utils.Message;
+
+import java.util.Iterator;
+
+public class DisplayService {
+    public static void show(Tournament tournament) {
+        Message.FOOTER.writeln();
+
+        System.out.println(tournament.getName());
+        System.out.println(tournament.getStartDate() + " - " + tournament.getEndDate());
+        System.out.println(tournament.getSport());
+        System.out.println(tournament.getLeague());
+
+        for (Match match : tournament.getMatches().getElements()) {
+            show(match);
+        }
+
+        Message.FOOTER.writeln();
+    }
+
+    public static void show(Match match) {
+        assert match != null;
+        assert match.getParticipants().size() > 1;
+
+        Iterator<Participant> iterator = match.getParticipants().iterator();
+
+        Message.LIGHT_FOOTER.writeln();
+        System.out.println(iterator.next());
+        do {
+            System.out.println("vs");
+            System.out.println(iterator.next());
+        } while (iterator.hasNext());
+        Message.LIGHT_FOOTER.writeln();
+    }
+
+    public static void show(Participant participant) {
+        System.out.println(participant.getName());
+
+        Statistics stats = participant.getStats();
+        for (Category category : Category.values()) {
+            System.out.println(" - " + category + ": " + stats.get(category));
+        }
+
+        for (Participant child : participant.getChildren()) {
+            show(child);
+        }
+    }
+}
