43360d143ba7abb05559e5878a9ac38c3f1af01d
Javier de Santiago <j.desantiagosoto@gmail.com>
2024-11-11 14:01:27 +0100

    feat!: transform Participant into an interface

--- src/main/java/es/upm/etsisi/models/entities/Participant.java
+++ src/main/java/es/upm/etsisi/models/entities/Participant.java
@@ -4,45 +4,9 @@ import es.upm.etsisi.models.game.Statistics;
 
 import java.util.ArrayList;
 
-public abstract class Participant {
-    private final String name;
-    private Statistics stats;
-    private String adminName;
-
-    public Participant(String name, Statistics stats, String adminName) {
-        this.name = name;
-        this.stats = stats;
-        this.adminName = adminName;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public Statistics getStats() {
-        return this.stats;
-    }
-
-    public void setStats(Statistics stats) {
-        this.stats = stats;
-    }
-
-    public abstract ArrayList<Participant> getChildren();
-
-    @Override
-    public boolean equals(Object object) {
-        if (this == object) {
-            return true;
-        }
-        if (object == null || getClass() != object.getClass()) {
-            return false;
-        }
-        Participant participant = (Participant) object;
-        return this.getName().equals(participant.getName());
-    }
-
-    @Override
-    public String toString() {
-        return this.getName();
-    }
+public interface Participant {
+    String getName();
+    Statistics getStats();
+    void setStats(Statistics stats);
+    ArrayList<Participant> getChildren();
 }
