e28e9df89752d26ec33a1f54110b4446653d2d1a
Javier de Santiago <j.desantiagosoto@gmail.com>
2024-11-11 14:03:06 +0100

    feat(Administrator): add role in constructor

--- src/main/java/es/upm/etsisi/models/auth/Administrator.java
+++ src/main/java/es/upm/etsisi/models/auth/Administrator.java
@@ -2,6 +2,6 @@ package es.upm.etsisi.models.auth;
 
 public class Administrator extends User {
     public Administrator(String username, String password) {
-        super(username, password);
+        super(username, password, Role.ADMIN);
     }
 }
