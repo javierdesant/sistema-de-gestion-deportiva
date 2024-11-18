6bc227a5393f5bd32bd68add1306b6289a86fca1
JhonyRetro <94227499+JhonyRetro@users.noreply.github.com>
2024-11-18 22:07:13 +0000

    feat: implement player-create command

--- src/main/java/es/upm/etsisi/commands/CreatePlayerCommand.java
+++ src/main/java/es/upm/etsisi/commands/CreatePlayerCommand.java
@@ -1,23 +1,36 @@
 package es.upm.etsisi.commands;
 
+import es.upm.etsisi.exceptions.DuplicateElementException;
+import es.upm.etsisi.exceptions.DuplicatePlayerException;
 import es.upm.etsisi.service.Controller;
+import es.upm.etsisi.utils.DNI;
 import es.upm.etsisi.utils.Message;
 
-public class CreatePlayerCommand extends Command {  // TODO: remake for 2.0.0
+public class CreatePlayerCommand extends Command {
     private final Controller controller;
 
     public CreatePlayerCommand(Controller controller) {
-        super("player-create", 0);
+        super("player-create", 5);
         this.controller = controller;
     }
 
     @Override
-    public void execute() {
-        String playerName = this.getArgument(0);
-
+    public void execute() throws DuplicateElementException{
+        String username = this.getArgument(0);
+        String password = this.getArgument(1);
+        String playerName = this.getArgument(2);
+        String playerLastName = this.getArgument(3);
+        DNI dni = new DNI(this.getArgument(4));
+        
         assert playerName.matches("[a-zA-Z]+") : Message.INVALID_NAME;
 
-//        this.entityList.add(new Player(playerName));      // FIXME
+        try {
+            this.controller.createPlayer(username, password, playerLastName, playerLastName, dni);
+        } catch (DuplicatePlayerException exception) {
+            exception.toString();
+            exception.printStackTrace();
+        }
+
         Message.PLAYER_ADDED.writeln();
     }
 }
