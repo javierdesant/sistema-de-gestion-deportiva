daad82d9d5cf470afc46ccca45980112a92cd7f3
JhonyRetro <94227499+JhonyRetro@users.noreply.github.com>
2024-11-18 22:17:00 +0000

    feat: implement team-create command

--- src/main/java/es/upm/etsisi/commands/CreateTeamCommand.java
+++ src/main/java/es/upm/etsisi/commands/CreateTeamCommand.java
@@ -1,6 +1,9 @@
 package es.upm.etsisi.commands;
 
+import es.upm.etsisi.exceptions.DuplicateElementException;
+import es.upm.etsisi.exceptions.DuplicateTeamException;
 import es.upm.etsisi.service.Controller;
+import es.upm.etsisi.utils.Message;
 
 public class CreateTeamCommand extends Command {
     private final Controller controller;
@@ -11,7 +14,18 @@ public class CreateTeamCommand extends Command {
     }
 
     @Override
-    public void execute() {
-        // TODO
+    public void execute() throws DuplicateElementException {
+        String teamName = this.getArgument(0);
+
+        assert teamName.matches("[a-zA-Z]+") : Message.INVALID_NAME;
+
+        try {
+            this.controller.createTeam(teamName);
+        } catch (DuplicateTeamException exception) {
+            exception.toString();
+            exception.printStackTrace();
+        }
+
+        Message.TEAM_ADDED.writeln();
     }
 }
