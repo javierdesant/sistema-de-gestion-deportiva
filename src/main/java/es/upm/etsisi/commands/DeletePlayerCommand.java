5720fc706df15e052b7c225549fccc012bb57d9a
JhonyRetro <94227499+JhonyRetro@users.noreply.github.com>
2024-11-18 22:46:07 +0000

    feat: implement player-delete command

--- src/main/java/es/upm/etsisi/commands/DeletePlayerCommand.java
+++ src/main/java/es/upm/etsisi/commands/DeletePlayerCommand.java
@@ -1,17 +1,25 @@
 package es.upm.etsisi.commands;
 
+import es.upm.etsisi.exceptions.NonExistElement;
 import es.upm.etsisi.service.Controller;
 
-public class DeletePlayerCommand extends Command {  // TODO: remake for 2.0.0
+public class DeletePlayerCommand extends Command {
     private final Controller controller;
 
     public DeletePlayerCommand(Controller controller) {
-        super("player-remove", 0);
+        super("player-remove", 1);
         this.controller = controller;
     }
 
     @Override
-    public void execute() {
-        // TODO
+    public void execute() throws NonExistElement {
+        String playerName = this.getArgument(0);
+
+        try {
+            this.controller.deleteParticipant(playerName);
+        } catch (NonExistElement exception) {
+            exception.toString();
+            exception.printStackTrace();
+        }
     }
 }
