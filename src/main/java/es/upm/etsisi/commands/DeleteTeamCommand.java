d1cc632e04ddbd73d8b5307a7d67aeae85ce5a9c
JhonyRetro <94227499+JhonyRetro@users.noreply.github.com>
2024-11-19 09:39:25 +0000

    feat: implement team-delete command

--- src/main/java/es/upm/etsisi/commands/DeleteTeamCommand.java
+++ src/main/java/es/upm/etsisi/commands/DeleteTeamCommand.java
@@ -1,17 +1,23 @@
 package es.upm.etsisi.commands;
 
+import es.upm.etsisi.exceptions.NonExistElement;
 import es.upm.etsisi.service.Controller;
+import es.upm.etsisi.utils.Message;
 
 public class DeleteTeamCommand extends Command {
     private final Controller controller;
 
     public DeleteTeamCommand(Controller controller) {
-        super("team-delete", 0);        // TODO: define Tournament and Match models
+        super("team-delete", 1);
         this.controller = controller;
     }
 
     @Override
-    public void execute() {
-        // TODO
+    public void execute() throws NonExistElement {
+        String teamName = this.getArgument(0);
+
+        this.controller.deleteParticipant(teamName);
+
+        Message.TEAM_REMOVED.writeln();
     }
 }
