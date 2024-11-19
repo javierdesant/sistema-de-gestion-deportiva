2923e75ee032c6a99523aee884dab2f206d8f04b
Javier de Santiago <j.desantiagosoto@gmail.com>
2024-11-19 11:05:09 +0100

    fix: make DifferingTypeException abstract

--- src/main/java/es/upm/etsisi/exceptions/DifferingTypeException.java
+++ src/main/java/es/upm/etsisi/exceptions/DifferingTypeException.java
@@ -1,8 +1,7 @@
 package es.upm.etsisi.exceptions;
 
-public class DifferingTypeException extends Exception{
-    public DifferingTypeException (String element, String type){
-        super("'" + element + "' no es del tipo '" + type + "'");
+public abstract class DifferingTypeException extends Exception {
+    public DifferingTypeException(String element, String type) {
+        super("'" + element + "' no es un '" + type + "'");
     }
-    
 }
