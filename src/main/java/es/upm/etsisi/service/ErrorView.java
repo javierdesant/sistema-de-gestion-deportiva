53262af0dbcffcdc8f3f8b4a9bcc1751c708595c
Javier de Santiago <j.desantiagosoto@gmail.com>
2024-11-22 18:54:15 +0100

    feat(ErrorView)!: transform MESSAGES static attribute into an EnumMap for consistency

--- src/main/java/es/upm/etsisi/service/ErrorView.java
+++ src/main/java/es/upm/etsisi/service/ErrorView.java
@@ -1,13 +1,23 @@
 package es.upm.etsisi.service;
 
+import java.util.EnumMap;
+
 public class ErrorView {
     private final Console console;
     private final Error error;
-    public static final String[] MESSAGES = {
-            "El elemento ya está en la lista",
-            "El elemento no se encuentra en la lista",
-            "Los nombres sólo pueden contener letras",
-            "Comando no válido",};
+
+    private static final EnumMap<Error, String> MESSAGES = new EnumMap<>(Error.class);
+
+    static {
+        MESSAGES.put(Error.DUPLICATE_ELEMENT_ERROR, "El elemento ya existe en la lista");
+        MESSAGES.put(Error.DUPLICATE_TOURNAMENT_ERROR, "El torneo ya existe en la lista");
+        MESSAGES.put(Error.DUPLICATE_MATCH_ERROR, "El partido ya existe en la lista");
+        MESSAGES.put(Error.PLAYER_ALREADY_IN_MATCH_ERROR, "El jugador ya está asignado a un partido");
+        MESSAGES.put(Error.ELEMENT_NOT_FOUND, "El elemento no se encuentra en la lista");
+        MESSAGES.put(Error.NAME_FORMAT_ERROR, "El nombre sólo puede contener letras");
+        MESSAGES.put(Error.PLAYER_NOT_IN_TEAM, "El jugador no pertenece al equipo");
+        MESSAGES.put(Error.INVALID_COMMAND, "Comando no válido");
+    }
 
     public ErrorView(Error error) {
         this.console = new Console();
@@ -15,6 +25,7 @@ public class ErrorView {
     }
 
     public void writeln() {
-        this.console.writeln(ErrorView.MESSAGES[this.error.ordinal()]);
+        String message = MESSAGES.getOrDefault(this.error, "Error desconocido");
+        this.console.writeln(message);
     }
 }
