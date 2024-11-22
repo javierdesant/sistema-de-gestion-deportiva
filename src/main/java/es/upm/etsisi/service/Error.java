f07cdc42ad52c5aa13694a6587be8cf703b1ea97
Javier de Santiago <j.desantiagosoto@gmail.com>
2024-11-22 18:15:18 +0100

    feat!: add new Error and ErrorView classes for common user misinput

--- src/main/java/es/upm/etsisi/service/Error.java
+++ src/main/java/es/upm/etsisi/service/Error.java
@@ -1,4 +1,8 @@
 package es.upm.etsisi.service;
 
 public enum Error {
+    DUPLICATE_ELEMENT_ERROR,
+    ELEMENT_NOT_FOUND,
+    NAME_FORMAT_ERROR,
+    INVALID_COMMAND,
 }
--- src/main/java/es/upm/etsisi/service/ErrorView.java
+++ src/main/java/es/upm/etsisi/service/ErrorView.java
@@ -1,4 +1,20 @@
 package es.upm.etsisi.service;
 
 public class ErrorView {
+    private final Console console;
+    private final Error error;
+    public static final String[] MESSAGES = {
+            "El elemento ya está en la lista",
+            "El elemento no se encuentra en la lista",
+            "Los nombres sólo pueden contener letras",
+            "Comando no válido",};
+
+    public ErrorView(Error error) {
+        this.console = new Console();
+        this.error = error;
+    }
+
+    public void writeln() {
+        this.console.writeln(ErrorView.MESSAGES[this.error.ordinal()]);
+    }
 }
