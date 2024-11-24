72dc52929aadf5359547a7476531052afc6c7a1e
Javier de Santiago <j.desantiagosoto@gmail.com>
2024-11-24 21:50:06 +0100

    chore(Command): optimize imports

--- src/main/java/es/upm/etsisi/commands/Command.java
+++ src/main/java/es/upm/etsisi/commands/Command.java
@@ -1,7 +1,7 @@
 package es.upm.etsisi.commands;
 
-import es.upm.etsisi.service.ErrorType;
 import es.upm.etsisi.service.CommandArguments;
+import es.upm.etsisi.service.ErrorType;
 
 import java.util.Collection;
 import java.util.Iterator;
