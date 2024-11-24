8416cbb3e92b7349bdd608e6a7d623ca7bb4f609
Javier de Santiago <j.desantiagosoto@gmail.com>
2024-11-24 22:50:27 +0100

    feat: implement LogoutCommand

--- src/main/java/es/upm/etsisi/commands/LogoutCommand.java
+++ src/main/java/es/upm/etsisi/commands/LogoutCommand.java
@@ -1,21 +1,21 @@
 package es.upm.etsisi.commands;
 
-import es.upm.etsisi.service.CLI;
+import es.upm.etsisi.service.CommandArguments;
 import es.upm.etsisi.service.Controller;
+import es.upm.etsisi.service.ErrorType;
 
 public class LogoutCommand extends Command {
     private final Controller controller;
-    private final CLI cli;
 
-    public LogoutCommand(Controller controller, CLI cli) {
+    public LogoutCommand(Controller controller) {
         super("logout", 0);
         this.controller = controller;
-        this.cli = cli;
     }
 
     @Override
-    public void execute() {
+    protected ErrorType execute(CommandArguments args) {
         this.controller.logout();
-        this.cli.updateCommands();
+
+        return null;
     }
 }
