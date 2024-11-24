b48c4d882dc3c009b76a2519e1955762d48d4fb0
Javier de Santiago <j.desantiagosoto@gmail.com>
2024-11-24 21:31:59 +0100

    feat!: add new CommandFactory class to manage command creation

--- src/main/java/es/upm/etsisi/service/CLI.java
+++ src/main/java/es/upm/etsisi/service/CLI.java
@@ -2,77 +2,22 @@ package es.upm.etsisi.service;
 
 import es.upm.etsisi.SportsManager;
 import es.upm.etsisi.commands.*;
-import es.upm.etsisi.models.Role;
 import es.upm.etsisi.models.User;
 import es.upm.etsisi.utils.Message;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.Scanner;
 
 public class CLI {
     private final SportsManager sportsManager; // move status to the CLI ?
-    private final LinkedList<Command> commands;
     private final Controller controller;
+    private final CommandFactory commandFactory;
     private final Scanner scanner;  // TODO: replace with console
 
     public CLI(SportsManager sportsManager) { // erase manager param ?
-        this.commands = new LinkedList<>();
-        this.controller = new Controller();
         this.sportsManager = sportsManager;
+        this.controller = new Controller();
+        this.commandFactory = new CommandFactory(this.controller);
         this.scanner = new Scanner(System.in);
-    }
-
-    public ArrayList<Command> getCommands() {
-        return new ArrayList<>(this.commands);
-    }
-
-    public void updateCommands() {
-        User user = this.controller.getUser();
-
-        this.commands.clear();
-
-        this.addPublicCommands();
-        if (user != null) {
-            if (user.getRole() == Role.ADMIN) {
-                this.addAdminCommands();
-            } else if (user.getRole() == Role.PLAYER) {
-                this.addPlayerCommands();
-            }
-        }
-    }
-
-    private void addAdminCommands() {
-        // this.add(new CreatePlayerCommand(this.controller));
-        // this.add(new CreateTeamCommand(this.controller));
-        // this.add(new DeletePlayerCommand(this.controller, this.tournamentList, new
-        // Scanner(System.in))); // FIXME
-        // this.add(new DeleteTeamCommand(this.controller));
-        this.add(new AddToTeamCommand(this.controller));
-        // this.add(new RemoveFromTeamCommand(this.controller));
-        // this.add(new CreateTournamentCommand()); // TODO
-        // this.add(new DeleteTournamentCommand()); // TODO
-        // this.add(new TournamentMatchmakingCommand() or MatchmakeCommand()); // TODO
-    }
-
-    private void addPlayerCommands() {
-        // this.add(new EnrollCommand());
-        // this.add(new LeaveCommand());
-        // this.add(new ShowCommand());
-        // add tournament related commands... // TODO
-        // this.add(new ShowStatsCommand()); // TODO
-    }
-
-    private void addPublicCommands() {
-        this.add(new LoginCommand(this.controller, this));
-        this.add(new LogoutCommand(this.controller, this));
-        this.add(new HelpCommand(this));
-        this.add(new ExitCommand(this.sportsManager));
-    }
-
-    private void add(Command command) {
-        this.commands.add(command);
     }
 
     public void run() {
@@ -101,21 +46,12 @@ public class CLI {
         User user = this.controller.getUser();
 
         System.out.println();
-        System.out.print(user == null ? "" : user + " # ");
+        System.out.print(user != null ? user + " # " : "");
         Message.COMMAND_PROMPT.write();
 
         String input = this.scanner.nextLine().trim();
+        String[] tokens = input.split(" ", 2);
 
-        Command res = null;
-        Iterator<Command> iterator = this.commands.iterator();
-        while (iterator.hasNext() && res == null) {
-            Command command = iterator.next();
-            if (command.isCalled(input)) {
-                command.validate(input);
-                res = command;
-            }
-        }
-
-        return res;
+        return this.commandFactory.getCommand(user != null ? user.getRole() : null, tokens[0]);
     }
 }
---
+++ src/main/java/es/upm/etsisi/service/CommandFactory.java
@@ -0,0 +1,72 @@
+package es.upm.etsisi.service;
+
+import es.upm.etsisi.commands.*;
+import es.upm.etsisi.models.Role;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+
+public class CommandFactory {
+    private final Controller controller;
+    private final HashMap<Role, ArrayList<Command>> commands;
+    private final ArrayList<Command> publicCommands;
+
+    public CommandFactory(Controller controller) {
+        this.controller = controller;
+        this.commands = new HashMap<>();
+        this.publicCommands = new ArrayList<>();
+
+        this.initializePublicCommands();
+        this.initializeCommands();
+    }
+
+    private void initializeCommands() {
+        this.commands.put(Role.ADMIN, createCommands(
+                new CreatePlayerCommand(this.controller),
+                new CreateTeamCommand(this.controller),
+                new DeletePlayerCommand(this.controller),
+                new DeleteTeamCommand(this.controller),
+                new AddToTeamCommand(this.controller),
+                new RemoveFromTeamCommand(this.controller),
+                new CreateTournamentCommand(this.controller),
+                new DeleteTournamentCommand(this.controller),
+                new MatchmakeCommand(this.controller)
+        ));
+
+        this.commands.put(Role.PLAYER, createCommands(
+                new EnrollCommand(this.controller),
+                new LeaveCommand(this.controller),
+                new ShowStatsCommand(this.controller)
+        ));
+    }
+
+    private void initializePublicCommands() {
+        this.publicCommands.add(new LoginCommand(this.controller));
+        this.publicCommands.add(new LogoutCommand(this.controller));
+        this.publicCommands.add(new HelpCommand(this.controller));
+        this.publicCommands.add(new ExitCommand(this.controller));
+    }
+
+    private ArrayList<Command> createCommands(Command... specificCommands) {
+        ArrayList<Command> commandList = new ArrayList<>(this.publicCommands);
+        commandList.addAll(Arrays.asList(specificCommands));
+        return commandList;
+    }
+
+    public Command getCommand(Role role, String title) {
+        ArrayList<Command> activeCommands = this.commands.getOrDefault(role, this.publicCommands);
+        Command res = null;
+
+        Iterator<Command> iterator = activeCommands.iterator();
+        while (iterator.hasNext() && res == null) {
+            Command command = iterator.next();
+            if (command.isCalled(title)) {
+                res = command;
+            }
+        }
+
+        return res;
+    }
+}
