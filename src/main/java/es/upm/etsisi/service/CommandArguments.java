044c625de76f4076e4fe2ed896ee9eb0c37f1d3e
Javier de Santiago <j.desantiagosoto@gmail.com>
2024-11-24 21:34:49 +0100

    feat: add new CommandArguments class

---
+++ src/main/java/es/upm/etsisi/service/CommandArguments.java
@@ -0,0 +1,39 @@
+package es.upm.etsisi.service;
+
+import java.util.Collection;
+import java.util.LinkedList;
+
+public class CommandArguments {
+    LinkedList<String> tokens;
+    LinkedList<String> flags;
+
+    public CommandArguments(Collection<String> arguments) {
+        this.tokens = new LinkedList<>(arguments);
+        this.flags = new LinkedList<>();
+        this.separateFlags();
+    }
+
+    private void separateFlags() {
+        LinkedList<String> remainingTokens = new LinkedList<>();
+
+        for (String token : this.tokens) {
+            if (token.startsWith("-")) {
+                this.flags.add(token);
+            } else {
+                remainingTokens.add(token);
+            }
+        }
+
+        this.tokens = remainingTokens;
+    }
+
+    public boolean containsFlag(String flag) {
+        assert flag.startsWith("-");
+        return this.flags.contains(flag);
+    }
+
+    public String pollToken() {
+        return this.tokens.poll();
+    }
+
+}
