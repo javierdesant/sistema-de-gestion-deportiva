d5bd5c589938f13150c0898a8667bfdd21e537d1
Javier de Santiago <j.desantiagosoto@gmail.com>
2024-11-25 19:06:00 +0100

    refactor(CLI): extract input parse logic into its own logic

--- src/main/java/es/upm/etsisi/service/CLI.java
+++ src/main/java/es/upm/etsisi/service/CLI.java
@@ -7,6 +7,7 @@ import es.upm.etsisi.views.commands.CommandFactory;
 import es.upm.etsisi.models.User;
 import es.upm.etsisi.utils.Message;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class CLI {
@@ -37,13 +38,11 @@ public class CLI {
         do {
             User user = this.controller.getUser();
             String input = this.readInput();
-            String[] split = input.trim().split(" ", 2);
-            String title = split[0];
-            String[] arguments = split.length > 1 ? split[1].split(";") : new String[0];
+            String[] parsedInput = this.splitInput(input);
 
-            Command command = this.commandFactory.getCommand(user != null ? user.getRole() : null, title);
+            Command command = this.commandFactory.getCommand(user != null ? user.getRole() : null, parsedInput[0]);
             if (command != null) {
-                error = command.execute(arguments);
+                error = command.execute(Arrays.copyOfRange(parsedInput, 1, parsedInput.length));
             } else {
                 error = ErrorType.INVALID_COMMAND;
             }
@@ -62,5 +61,17 @@ public class CLI {
         Message.COMMAND_PROMPT.write();
 
         return this.scanner.nextLine();
+    }
+
+    private String[] splitInput(String input) {
+        String[] split = input.trim().split(" ", 2);
+        String title = split[0];
+        String[] arguments = split.length > 1 ? split[1].split(";") : new String[0];
+
+        String[] result = new String[arguments.length + 1];
+        result[0] = title;
+        System.arraycopy(arguments, 0, result, 1, arguments.length);
+
+        return result;
     }
 }
