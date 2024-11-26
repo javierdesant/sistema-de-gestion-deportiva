d154874e99466af8c7232fd0aa2d27b6a761ccb5
Javier de Santiago <j.desantiagosoto@gmail.com>
2024-11-26 13:05:44 +0100

    feat(TimeFrame): overcharge class constructor

--- src/main/java/es/upm/etsisi/models/TimeFrame.java
+++ src/main/java/es/upm/etsisi/models/TimeFrame.java
@@ -1,10 +1,31 @@
 package es.upm.etsisi.models;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
-public record TimeFrame(LocalDate startDate, LocalDate endDate) {
-    public TimeFrame {
+public class TimeFrame {
+    private final LocalDate startDate;
+    private final LocalDate endDate;
+
+    public TimeFrame(LocalDate startDate, LocalDate endDate) {
         assert startDate.isBefore(endDate);
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public TimeFrame(String startDate, String endDate) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        this.startDate = LocalDate.parse(startDate, formatter);
+        this.endDate = LocalDate.parse(endDate, formatter);
+    }
+
+    public LocalDate getStartDate() {
+        return this.startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return this.endDate;
     }
 
     public boolean includes(LocalDate date) {
