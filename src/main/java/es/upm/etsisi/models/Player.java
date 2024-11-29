5b32a10dc2cf87c3739d86d5fdcef50c44b8df5b
JhonyRetro <jhonan989@gmail.com>
2024-11-29 14:40:03 +0100

    fix: fix DNI value comparison

--- src/main/java/es/upm/etsisi/models/Player.java
+++ src/main/java/es/upm/etsisi/models/Player.java
@@ -83,7 +83,7 @@ public class Player extends User implements Participant {
             return false;
         }
         Player player = (Player) object;
-        return this.dni.equals(player.getDni());
+        return this.dni.getValue().equals(player.getDni().getValue());
     }
 
     @Override
