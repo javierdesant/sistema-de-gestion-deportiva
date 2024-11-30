8d73cefa3469e20fbffd791693bb1e7aca115f6e
Javier de Santiago <j.desantiagosoto@gmail.com>
2024-11-30 19:42:20 +0100

    feat!: add EmailAddress and EmailValidator classes

---
+++ src/main/java/es/upm/etsisi/utils/EmailAddress.java
@@ -0,0 +1,38 @@
+package es.upm.etsisi.utils;
+
+public class EmailAddress {
+    private final String email;
+
+    protected EmailAddress(String email) {
+        this.email = email;
+    }
+
+    public static EmailAddress valueOf(String email, EmailValidator validator) {
+        if (!validator.isValid(email)) {
+            throw new IllegalArgumentException("Invalid email address: " + email);
+        }
+        return new EmailAddress(email);
+    }
+
+    @Override
+    public String toString() {
+        return this.email;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (object == null || getClass() != object.getClass()) {
+            return false;
+        }
+        EmailAddress that = (EmailAddress) object;
+        return this.email.equalsIgnoreCase(that.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return this.email.toLowerCase().hashCode();
+    }
+}
