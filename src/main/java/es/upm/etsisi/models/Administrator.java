34df4252f17f9d240cc235ac7b337328b1676d39
Javier de Santiago <j.desantiagosoto@gmail.com>
2024-11-30 21:28:45 +0100

    fix(Administrator): delete outdated assertion

--- src/main/java/es/upm/etsisi/models/Administrator.java
+++ src/main/java/es/upm/etsisi/models/Administrator.java
@@ -5,6 +5,5 @@ import es.upm.etsisi.utils.UpmEmail;
 public class Administrator extends User {
     public Administrator(UpmEmail username, String password) {
         super(username, password, Role.ADMIN);
-        assert this.hasUpmEmail();
     }
 }
