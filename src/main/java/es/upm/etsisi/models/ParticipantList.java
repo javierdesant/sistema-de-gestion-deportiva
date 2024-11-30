950958507922290a17fed79de4b3b75ff19e1d74
Javier de Santiago <j.desantiagosoto@gmail.com>
2024-12-01 00:16:39 +0100

    refactor

--- src/main/java/es/upm/etsisi/models/ParticipantList.java
+++ src/main/java/es/upm/etsisi/models/ParticipantList.java
@@ -22,13 +22,16 @@ public class ParticipantList extends List<Participant> {
     @Override
     public boolean contains(Participant participant) {
         boolean found = super.contains(participant);
+
         if (!participant.hasChildren()) {
             Player player = (Player) participant;
+
             Iterator<Participant> iterator = this.iterator();
             while (iterator.hasNext() && !found) {
                 found = iterator.next().contains(player);
             }
         }
+
         return found;
     }
 
