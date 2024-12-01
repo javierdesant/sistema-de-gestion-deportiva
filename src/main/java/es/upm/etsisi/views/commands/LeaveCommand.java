af5edbfc4f9eaa45b433eba6f6d394e939d39ccb
Javier de Santiago <j.desantiagosoto@gmail.com>
2024-12-01 03:07:44 +0100

    feat: implement LeaveCommand

--- src/main/java/es/upm/etsisi/views/commands/LeaveCommand.java
+++ src/main/java/es/upm/etsisi/views/commands/LeaveCommand.java
@@ -2,18 +2,34 @@ package es.upm.etsisi.views.commands;
 
 import es.upm.etsisi.service.Controller;
 import es.upm.etsisi.service.ErrorType;
+import es.upm.etsisi.utils.Message;
 
 public class LeaveCommand extends Command {
     private final Controller controller;
 
     LeaveCommand(Controller controller) {
-        super("tournament-remove", 0, "Da de baja al jugador autenticado o a su equipo de un torneo.");
+        super("tournament-remove", 2, "Da de baja al jugador autenticado o a su equipo de un torneo.");
         this.controller = controller;
     }
 
     @Override
     protected ErrorType execute(CommandArguments args) {
-        // TODO
-        return ErrorType.NULL;
+        ErrorType error;
+        String tournamentName = args.pollToken();
+
+        if (this.areInvalidTokens(tournamentName)) {
+            return ErrorType.INVALID_ARGUMENTS;
+        }
+
+        if (args.containsFlag("-t")) {
+            error = this.controller.leaveTournamentAsTeam(tournamentName);
+        } else {
+            error = this.controller.leaveTournament(tournamentName);
+        }
+
+        if (error.isNull()) {
+            Message.PLAYER_ADDED.writeln();
+        }
+        return error;
     }
 }
