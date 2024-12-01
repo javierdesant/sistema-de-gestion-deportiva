f38142011894223b1b54237c0221d7dce9c73544
Javier de Santiago <j.desantiagosoto@gmail.com>
2024-12-01 02:33:18 +0100

    feat: implement EnrollCommand

--- src/main/java/es/upm/etsisi/views/commands/EnrollCommand.java
+++ src/main/java/es/upm/etsisi/views/commands/EnrollCommand.java
@@ -2,18 +2,34 @@ package es.upm.etsisi.views.commands;
 
 import es.upm.etsisi.service.Controller;
 import es.upm.etsisi.service.ErrorType;
+import es.upm.etsisi.utils.Message;
 
 public class EnrollCommand extends Command {
     private final Controller controller;
 
     EnrollCommand(Controller controller) {
-        super("tournament-add", 0, "Inscribe al jugador autenticado o a su equipo en un torneo.");
+        super("tournament-add", 2, "Inscribe al jugador autenticado o a su equipo en un torneo.");
         this.controller = controller;
     }
 
     @Override
     protected ErrorType execute(CommandArguments args) {
-        // TODO
-        return ErrorType.NULL;
+        ErrorType error;
+        String tournamentName = args.pollToken();
+
+        if (this.areInvalidTokens(tournamentName)) {
+            return ErrorType.INVALID_ARGUMENTS;
+        }
+
+        if (args.containsFlag("-t")) {
+            error = this.controller.enrollTeamOfUser(tournamentName);
+        } else {
+            error = this.controller.enrollUser(tournamentName);
+        }
+
+        if (error.isNull()) {
+            Message.PLAYER_ADDED.writeln();
+        }
+        return error;
     }
 }
