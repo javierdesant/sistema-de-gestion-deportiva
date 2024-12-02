b4f7ed0eaef08259db4c1f5cfac1766c20b196b4
JhonyRetro <jhonan989@gmail.com>
2024-12-02 14:24:28 +0100

    refactor: refactor TournamentService in ListTournamentsCommand class

--- src/main/java/es/upm/etsisi/views/commands/ListTournamentsCommand.java
+++ src/main/java/es/upm/etsisi/views/commands/ListTournamentsCommand.java
@@ -1,20 +1,19 @@
 package es.upm.etsisi.views.commands;
 
-import es.upm.etsisi.models.TournamentList;
-import es.upm.etsisi.service.Controller;
 import es.upm.etsisi.service.ErrorType;
+import es.upm.etsisi.service.TournamentManager;
 
 public class ListTournamentsCommand extends Command {
-    private final Controller controller;
+    private final TournamentManager tournamentManager;
 
-    ListTournamentsCommand(Controller controller) {
+    ListTournamentsCommand(TournamentManager tournamentManager) {
         super("tournaments", 0, "Lista los torneos del sistema.");
-        this.controller = controller;
+        this.tournamentManager = tournamentManager;
     }
 
     @Override
     protected ErrorType execute(CommandArguments args) {
-        TournamentList tournaments = this.controller.getTournaments();
+        // TournamentList tournaments = this.controller.getTournaments();
 
         // TODO
         return ErrorType.NULL;
