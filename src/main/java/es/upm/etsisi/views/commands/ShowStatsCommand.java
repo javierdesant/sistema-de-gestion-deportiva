2f77b0a1cb706af788af51cd3e97ab1477d684e7
JhonyRetro <jhonan989@gmail.com>
2024-12-02 14:27:26 +0100

    refactor: refactor ParticipantService in ShowStatsCommand class

--- src/main/java/es/upm/etsisi/views/commands/ShowStatsCommand.java
+++ src/main/java/es/upm/etsisi/views/commands/ShowStatsCommand.java
@@ -4,29 +4,30 @@ import es.upm.etsisi.models.Category;
 import es.upm.etsisi.models.Player;
 import es.upm.etsisi.models.Role;
 import es.upm.etsisi.models.Statistics;
-import es.upm.etsisi.service.Controller;
 import es.upm.etsisi.service.ErrorType;
+import es.upm.etsisi.service.ParticipantManager;
 
 public class ShowStatsCommand extends Command {
-    private final Controller controller;
+    private final ParticipantManager participantManager;
 
-    ShowStatsCommand(Controller controller) {
-        super("statistics-show", 0, "Muestra las estadísticas del jugador autenticado. Con la opción -csv, se presentan en formato tabla; con -json, en formato clave-valor.");
-        this.controller = controller;
+    ShowStatsCommand(ParticipantManager participantManager) {
+        super("statistics-show", 0,
+                "Muestra las estadísticas del jugador autenticado. Con la opción -csv, se presentan en formato tabla; con -json, en formato clave-valor.");
+        this.participantManager = participantManager;
     }
 
     @Override
-    protected ErrorType execute(CommandArguments args) {    // TODO
-        assert this.controller.getUser().getRole() == Role.PLAYER;
+    protected ErrorType execute(CommandArguments args) { // TODO
+        /*assert this.participantManager.getUser().getRole() == Role.PLAYER;
 
-        Player player = (Player) this.controller.getUser();
+        Player player = (Player) this.participantManager.getUser();
 
-        Statistics stats = this.controller.getParticipantStats(player);
+        Statistics stats = this.participantManager.getParticipantStats(player);
 
         for (Category category : Category.values()) {
             System.out.println(category + "\t- " + stats.get(category));
         }
-
-        return ErrorType.NULL;
+        */
+        return ErrorType.NULL; 
     }
 }
