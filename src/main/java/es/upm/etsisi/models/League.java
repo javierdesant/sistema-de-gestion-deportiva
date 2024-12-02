29a0c871b48b31deb6597d05bf47036f00b840f7
JhonyRetro <94227499+JhonyRetro@users.noreply.github.com>
2024-12-02 18:25:17 +0100

    feat!: add code key to tournament enums

--- src/main/java/es/upm/etsisi/models/League.java
+++ src/main/java/es/upm/etsisi/models/League.java
@@ -1,28 +1,34 @@
 package es.upm.etsisi.models;
 
 public enum League {
-    U10("Liga Sub-10"),
-    U12("Liga Sub-12"),
-    U14("Liga Sub-14"),
-    U16("Liga Sub-16"),
-    U18("Liga Sub-18"),
-    U20("Liga Sub-20"),
-    U23("Liga Sub-23"),
-    ADULT("Liga de Adultos"),
-    MASTER("Liga de Veteranos"),
-    SENIOR("Liga Senior"),
-    OVER_40("Liga para Mayores de 40"),
-    OVER_50("Liga para Mayores de 50"),
-    OVER_60("Liga para Mayores de 60");
+    U10("Liga Sub-10", "U10"),
+    U12("Liga Sub-12", "U12"),
+    U14("Liga Sub-14", "U14"),
+    U16("Liga Sub-16", "U16"),
+    U18("Liga Sub-18", "U18"),
+    U20("Liga Sub-20", "U20"),
+    U23("Liga Sub-23", "U23"),
+    ADULT("Liga de Adultos", "ADULT"),
+    MASTER("Liga de Veteranos", "MASTER"),
+    SENIOR("Liga Senior", "SENIOR"),
+    OVER_40("Liga para Mayores de 40", "40PLUS"),
+    OVER_50("Liga para Mayores de 50", "50PLUS"),
+    OVER_60("Liga para Mayores de 60", "60PLUS");
 
     private final String title;
+    private final String code;
 
-    League(String title) {
+    League(String title, String code) {
         this.title = title;
+        this.code = code;
     }
 
     @Override
     public String toString() {
         return title;
+    }
+
+    public String getCode() {
+        return code;
     }
 }
