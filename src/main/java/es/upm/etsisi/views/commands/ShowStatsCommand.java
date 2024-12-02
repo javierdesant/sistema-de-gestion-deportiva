560c0edfbe6c41f022d06ce68fdf356262cb1c15
JhonyRetro <94227499+JhonyRetro@users.noreply.github.com>
2024-12-02 16:48:16 +0100

    feat: implement statistics-show command

--- src/main/java/es/upm/etsisi/views/commands/ShowStatsCommand.java
+++ src/main/java/es/upm/etsisi/views/commands/ShowStatsCommand.java
@@ -1,33 +1,28 @@
 package es.upm.etsisi.views.commands;
 
-import es.upm.etsisi.models.Category;
-import es.upm.etsisi.models.Player;
-import es.upm.etsisi.models.Role;
-import es.upm.etsisi.models.Statistics;
 import es.upm.etsisi.service.ErrorType;
-import es.upm.etsisi.service.ParticipantManager;
+import es.upm.etsisi.service.ParticipantService;
 
 public class ShowStatsCommand extends Command {
-    private final ParticipantManager participantManager;
+    private final ParticipantService participantService;
 
-    ShowStatsCommand(ParticipantManager participantManager) {
-        super("statistics-show", 0,
+    ShowStatsCommand(ParticipantService participantService) {
+        super("statistics-show", 1,
                 "Muestra las estadísticas del jugador autenticado. Con la opción -csv, se presentan en formato tabla; con -json, en formato clave-valor.");
-        this.participantManager = participantManager;
+        this.participantService = participantService;
     }
 
     @Override
-    protected ErrorType execute(CommandArguments args) { // TODO
-        /*assert this.participantManager.getUser().getRole() == Role.PLAYER;
+    protected ErrorType execute(CommandArguments args) {
+        ErrorType error;
 
-        Player player = (Player) this.participantManager.getUser();
-
-        Statistics stats = this.participantManager.getParticipantStats(player);
-
-        for (Category category : Category.values()) {
-            System.out.println(category + "\t- " + stats.get(category));
+        if (args.containsFlag("-csv")) {
+            error = this.participantService.showStatisticsCsv();
+        } else if (args.containsFlag("-json")) {
+            error = this.participantService.showStatisticsJson();
+        } else {
+            error = ErrorType.INVALID_ARGUMENTS;
         }
-        */
-        return ErrorType.NULL; 
+        return error;
     }
 }
