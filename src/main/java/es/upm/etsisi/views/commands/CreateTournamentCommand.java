c8be2b8e2cc559e687682a28550f4c92fc9214dd
JhonyRetro <jhonan989@gmail.com>
2024-12-02 14:21:49 +0100

    refactor: refactor TournamentService in CreateTournamentCommand class

--- src/main/java/es/upm/etsisi/views/commands/CreateTournamentCommand.java
+++ src/main/java/es/upm/etsisi/views/commands/CreateTournamentCommand.java
@@ -1,14 +1,14 @@
 package es.upm.etsisi.views.commands;
 
-import es.upm.etsisi.service.Controller;
 import es.upm.etsisi.service.ErrorType;
+import es.upm.etsisi.service.TournamentService;
 
-public class CreateTournamentCommand extends Command {  // TODO: finish pending Tournament model
-    private final Controller controller;
+public class CreateTournamentCommand extends Command { // TODO: finish pending Tournament model
+    private final TournamentService tournamentService;
 
-    CreateTournamentCommand(Controller controller) {    //TODO: add args to description
+    CreateTournamentCommand(TournamentService tournamentService) { // TODO: add args to description
         super("tournament-create", 0, "Registra un nuevo torneo en el sistema.");
-        this.controller = controller;
+        this.tournamentService = tournamentService;
     }
 
     @Override
