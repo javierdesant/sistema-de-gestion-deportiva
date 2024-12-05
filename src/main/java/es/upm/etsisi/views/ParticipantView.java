2c7b3f1ec30894d4cfae8ae8f5a2bcd3841d1fd0
Cafileiro <riralde@gmail.com>
2024-12-05 15:01:53 +0100

    fix(participantView): now differents participants are reconized

--- src/main/java/es/upm/etsisi/views/ParticipantView.java
+++ src/main/java/es/upm/etsisi/views/ParticipantView.java
@@ -1,30 +1,40 @@
 package es.upm.etsisi.views;
 
-
 import es.upm.etsisi.utils.Console;
 import es.upm.etsisi.models.Participant;
 import es.upm.etsisi.models.Statistics;
 import es.upm.etsisi.models.Category;
+
 public class ParticipantView extends View<Participant> {
 
     private ParticipantView(){
-        super()
+        super();
     }
 
     @Override
     public void write(Participant element) {
-        //comprobar si llega equipo o jugador
-        Console.getInstance().writeln("     JUGADOR     ");
-        Console.getInstance().writeln(element.getName());
-        Statistics statistics = element.getStats();
-        Console.getInstance().writeln("     ESTADISTICAS     ");
-        Console.getInstance().write("----------------------");
-        for(Category category : Category.values()){
 
-            Console.getInstance().write(category.name() + ": ");
-            Console.getInstance().writeln(statistics.get(category));
+        if(!element.hasChildren()){
+
+            Console.getInstance().writeln("     JUGADOR     ");
+            Console.getInstance().writeln(element.getName());
+            Statistics statistics = element.getStats();
+            Console.getInstance().writeln("     ESTADISTICAS     ");
+            Console.getInstance().write("----------------------");
+            for(Category category : Category.values()){
+
+                Console.getInstance().write(category.name() + ": ");
+                Console.getInstance().writeln(statistics.get(category));
+
+            }
+            Console.getInstance().writeln("----------------------");
+        }else{
+            writelnConsole("EQUIPO " + element.getName());
+            writelnConsole("----------------------");
+            for (Participant player : element.getChildren()) {
+                write(player);
+            }
         }
-        Console.getInstance().writeln("----------------------");
-        
+
     }
 }
