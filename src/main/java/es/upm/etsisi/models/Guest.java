79f1f239b9b8970fad97abe93ed67055c02a952f
Javier de Santiago <j.desantiagosoto@gmail.com>
2024-12-05 23:02:01 +0100

    refactor: improve Guest class

--- src/main/java/es/upm/etsisi/models/Guest.java
+++ src/main/java/es/upm/etsisi/models/Guest.java
@@ -1,13 +1,16 @@
 package es.upm.etsisi.models;
 
 public class Guest extends User {
-    public static final Guest instance = new Guest();
+    public static Guest instance;
 
     private Guest() {
         super(null, null, Role.GUEST);
     }
 
     public static Guest getInstance() {
+        if (instance == null) {
+            instance = new Guest();
+        }
         return instance;
     }
 }
