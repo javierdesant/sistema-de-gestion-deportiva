b5aefaa182c32d5d0f6b43d3e653cbf616770fc4
Javier de Santiago <j.desantiagosoto@gmail.com>
2024-12-06 01:56:26 +0100

    feat: add more specific error codes in CreateTournamentCommand

--- src/main/java/es/upm/etsisi/views/commands/CreateTournamentCommand.java
+++ src/main/java/es/upm/etsisi/views/commands/CreateTournamentCommand.java
@@ -13,10 +13,7 @@ public class CreateTournamentCommand extends Command {
 
     CreateTournamentCommand(TournamentService tournamentService) {
         super("tournament-create", 6,
-                "[name;sport;league;category;startDate;endDate] Registra un nuevo torneo en el sistema.\n" +
-                        "\tLos códigos de Deporte, Liga y Categoría deben ser del formato correspondiente según el documento tipo_code_references en la carpeta docs.\n"
-                        +
-                        "\tEl formato de la fecha debe ser dd/mm/aaaa.\n");
+                "[name;sportCode;leagueCode;categoryCode;startDate;endDate] Registra un nuevo torneo en el sistema.");
         this.tournamentService = tournamentService;
     }
 
@@ -24,18 +21,18 @@ public class CreateTournamentCommand extends Command {
     protected ErrorType execute(ParsedInput args) {
         ErrorType error;
         String name = args.pollToken();
-        String sport = args.pollToken();
-        String league = args.pollToken();
-        String category = args.pollToken();
+        String sportCode = args.pollToken();
+        String leagueCode = args.pollToken();
+        String categoryCode = args.pollToken();
         String startDate = args.pollToken();
         String endDate = args.pollToken();
 
-        error = this.validate(name, sport, league, category, startDate, endDate);
+        error = this.validate(name, sportCode, leagueCode, categoryCode, startDate, endDate);
         if (!error.isNull()) {
             return error;
         }
-        error = this.tournamentService.createTournament(new TournamentInfo(name, Sport.getFromCode(sport),
-                League.getFromCode(league), Category.getFromCode(category)), new TimeFrame(startDate, endDate));
+        error = this.tournamentService.createTournament(new TournamentInfo(name, Sport.getFromCode(sportCode),
+                League.getFromCode(leagueCode), Category.getFromCode(categoryCode)), new TimeFrame(startDate, endDate));
         if (error.isNull()) {
             CommandFeedback.TOURNAMENT_ADDED.writeln();
         }
@@ -43,29 +40,28 @@ public class CreateTournamentCommand extends Command {
         return error;
     }
 
-    private ErrorType validate(String name, String sport, String league, String category, String startDate,
-                               String endDate) {
-        if (this.areInvalidTokens(name, sport, league, category, startDate, endDate)) {
+    private ErrorType validate(String name, String sportCode, String leagueCode, String categoryCode, String startDate, String endDate) {
+        if (this.areInvalidTokens(name, sportCode, leagueCode, categoryCode, startDate, endDate)) {
             return ErrorType.INVALID_ARGUMENTS;
-        } else if (Sport.getFromCode(sport) == null || League.getFromCode(league) == null
-                || Category.getFromCode(category) == null) {
-            return ErrorType.INVALID_ARGUMENTS;
-        } else if (!validateDate(startDate).isNull() || !validateDate(endDate).isNull()) {
-            return ErrorType.INVALID_ARGUMENTS;
-        } else if (LocalDate.parse(endDate, DateTimeFormatter.ofPattern("dd/MM/yyyy"))
-                .isBefore(LocalDate.parse(startDate, DateTimeFormatter.ofPattern("dd/MM/yyyy")))) {
-            return ErrorType.INVALID_ARGUMENTS;
-        } else {
-            return ErrorType.NULL;
-        }
-    }
-
-    private ErrorType validateDate(String date) {
-        if (!date.matches("^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/\\d{4}$")) {
+        } else if (this.cantFindCodes(sportCode, leagueCode, categoryCode)) {
+            return ErrorType.INVALID_CODE;
+        } else if (this.isNotValidDateFormat(startDate) || this.isNotValidDateFormat(endDate)) {
             return ErrorType.DATE_FORMAT_ERROR;
+        } else if (!TimeFrame.isValidFrame(startDate, endDate)) {
+            return ErrorType.INVALID_ARGUMENTS;
         } else {
             return ErrorType.NULL;
         }
     }
 
+    private boolean isNotValidDateFormat(String date) {
+        String dateRegex = "^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/\\d{4}$";
+        return !date.matches(dateRegex);
+    }
+
+    private boolean cantFindCodes(String sportCode, String leagueCode, String categoryCode) {
+        return Sport.getFromCode(sportCode) == null ||
+                League.getFromCode(leagueCode) == null ||
+                Category.getFromCode(categoryCode) == null;
+    }
 }
