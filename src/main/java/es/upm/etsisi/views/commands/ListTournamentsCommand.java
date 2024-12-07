3f8bbf9d0c8a58331fd32cfd44c471643fad2843
JhonyRetro <94227499+JhonyRetro@users.noreply.github.com>
2024-12-07 19:57:10 +0100

    fix: replace incorrect types

--- src/main/java/es/upm/etsisi/views/commands/ListTournamentsCommand.java
+++ src/main/java/es/upm/etsisi/views/commands/ListTournamentsCommand.java
@@ -1,14 +1,14 @@
 package es.upm.etsisi.views.commands;
 
 import es.upm.etsisi.service.ErrorType;
-import es.upm.etsisi.service.TournamentManager;
+import es.upm.etsisi.service.TournamentService;
 
 public class ListTournamentsCommand extends Command {
-    private final TournamentManager tournamentService;
+    private final TournamentService tournamentService;
 
-    ListTournamentsCommand(TournamentManager tournamentManager) {
+    ListTournamentsCommand(TournamentService tournamentService) {
         super("tournament-list", 0, "Lista los torneos del sistema.");
-        this.tournamentService = tournamentManager;
+        this.tournamentService = tournamentService;
     }
 
     @Override
